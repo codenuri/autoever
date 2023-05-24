ca7d186a4433d8732ea8a0b40fcd5facb611e018
ecourse <smkang@codenuri.co.kr>
2023-05-24 13:39:40 +0900

    Add files via upload

--- DAY1/06_jthread4.cpp
+++ DAY1/06_jthread4.cpp
@@ -3,19 +3,32 @@
 #include <thread>
 using namespace std::literals;
 
-void foo()
+void foo(bool& quitFlag)
 {
     for (int i = 0; i < 10; i++)
     {
         std::this_thread::sleep_for(500ms);
         std::cout << "foo : " << i << std::endl;
+
+        // 스레드는 주기적으로 "flag" 를 조사해야 합니다.
+        if (quitFlag == true)
+            break;
+
     }
+    // 사용하던 자원은 안전하게 정리하고 종료
+    std::cout << "finish...foo\n";
 }
+
+
 int main()
 {
-    std::thread t(foo);
+    bool quitFlag = false;
+
+    std::thread t(foo, std::ref(quitFlag) );
 
     std::this_thread::sleep_for(2s);
+
+    quitFlag = true;
 
     t.join();
 }
