fcedeb5c08f6914c37082f0fe8ee85ee9152d46b
ecourse <smkang@codenuri.co.kr>
2023-05-24 15:25:25 +0900

    Add files via upload

--- DAY1/07_promise1.cpp
+++ DAY1/07_promise1.cpp
@@ -14,11 +14,22 @@ int add1(int a, int b)
 }
 
 // 방법 1. 스레드로 수행한 결과를 얻을때는 out parameter 를 사용
+// => 단점 : 주스레드에서 연산이 완료된 시점을 알수 없습니다.
+// => 스레드의 종료를 대기할수 있지만
+// => 연산의 종료를 대기할수 없습니다.
+
+// 해결책
+// 1. promise/future 모델
+// 2. packaged_task
+// 3. async
+// 4. 직접 동기화 객체 사용(mutex, condition_variable)
 void add2(int a, int b, int& s)
 {
 	s = a + b;
-}
 
+	// 연산은 종료 되었지만.. 마무리 작업에 시간이 걸린다고 가정
+	std::this_thread::sleep_for(3s);
+}
 
 int main()
 {
