bc3e97dbf28e5ffdebf783226b358c73e299806f
ecourse <smkang@codenuri.co.kr>
2023-05-24 13:41:10 +0900

    Add files via upload

--- DAY1/06_jthread4.cpp
+++ DAY1/06_jthread4.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <thread>
 using namespace std::literals;
-
+// 아래 코드를 
+// C# 언어에서 "협력적 취소(coperative cancelation)" 이라고합니다
 void foo(bool& quitFlag)
 {
     for (int i = 0; i < 10; i++)
@@ -13,7 +14,6 @@ void foo(bool& quitFlag)
         // 스레드는 주기적으로 "flag" 를 조사해야 합니다.
         if (quitFlag == true)
             break;
-
     }
     // 사용하던 자원은 안전하게 정리하고 종료
     std::cout << "finish...foo\n";
