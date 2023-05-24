7cdd6edd6793819795931616072fd879d90976c8
ecourse <smkang@codenuri.co.kr>
2023-05-24 09:31:43 +0900

    Add files via upload

--- DAY1/02_reference_wrapper1.cpp
+++ DAY1/02_reference_wrapper1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <functional>
 // C++ 레퍼런스 : 값의 이동, 레퍼런스 자체는 이동될수 없다
+// reference_wrapper : 이동가능한 참조
+//					   대입연산시 참조가 이동
 int main()
 {
 	int v1 = 10, v2 = 20;
@@ -12,9 +14,9 @@ int main()
 
 	r1 = r2;
 
-	// 결과 예측해 보세요.        // raw reference
-	std::cout << v1 << std::endl; // 20
-	std::cout << v2 << std::endl; // 20
-	std::cout << r1 << std::endl; // 20
-	std::cout << r2 << std::endl; // 20
+	// 결과 예측해 보세요.        // raw reference  reference_wrapper
+	std::cout << v1 << std::endl; // 20				10
+	std::cout << v2 << std::endl; // 20				20
+	std::cout << r1 << std::endl; // 20				20
+	std::cout << r2 << std::endl; // 20				20
 }
