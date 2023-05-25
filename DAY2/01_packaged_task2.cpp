e4d58da99d38fb12a4c297279dc466d1c49e03fc
ecourse <smkang@codenuri.co.kr>
2023-05-25 09:10:59 +0900

    Add files via upload

--- DAY2/01_packaged_task2.cpp
+++ DAY2/01_packaged_task2.cpp
@@ -18,10 +18,20 @@ int main()
 
 	// 3. task 를 호출(주스레드)하거나, 스레드로 실행합니다.
 	// task : () 연산자가 재정의 되어 있어서 함수처럼 사용가능
-	task(1, 2);
+	// task(1, 2); // 주스레드가 직접 호출한것
+
+	// std::thread t(task, 1, 2); // error.  
+							// task 는 복사 금지 이므로
+							// std::ref(), std::move() 사용
+	
+	// std::ref(task) : task 를 계속 사용하고싶을때
+	// std::move(task) : task 객체가 더이상 필요 없을때
+	std::thread t(std::ref(task), 1, 2);
 
 	int ret = ft.get();
 
 	std::cout << ret << std::endl;
+
+	t.join();
 }
 
