96f2dbcd18f50beeb35caaf9cd179b927431b406
ecourse <smkang@codenuri.co.kr>
2023-05-25 14:39:30 +0900

    Add files via upload

---
+++ DAY2/13_condition2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+using namespace std::literals;
+
+// condition_variable 사용법
+// 1. 헤더 포함
+#include <condition_variable>
+
+// 2. 전역변수 생성
+std::condition_variable cv;
+
+
+std::mutex m;
+int shared_data = -1; 
+
+
+
+void consumer()
+{
+    // 소비자는
+    // 3. unique_lock 으로 mutex 획득후..
+    std::unique_lock<std::mutex> ul(m);
+
+    // 4. cv.wait()로 신호를 대기 합니다.
+    cv.wait(ul);
+
+    std::cout << "consume : " << shared_data << std::endl;
+}
+void producer()
+{
+    std::this_thread::sleep_for(10ms);
+
+    {
+        std::lock_guard<std::mutex> lg(m);
+        shared_data = 100;
+        std::cout << "produce : " << shared_data << std::endl;
+    }
+
+    // 5. 생산자는 생산이 끝나면 lock 을 풀고 신호를 발생합니다.
+    cv.notify_one();
+}
+
+
+int main()
+{
+    std::thread t1(producer);
+    std::thread t2(consumer);
+    t1.join();
+    t2.join();
+}
