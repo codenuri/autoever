224fcff47d6c60d54d0a7f11d495771eb5044e19
ecourse <smkang@codenuri.co.kr>
2023-05-25 13:23:16 +0900

    Add files via upload

--- DAY2/09_lock_guard1.cpp
+++ DAY2/09_lock_guard1.cpp
@@ -3,6 +3,13 @@
 #include <mutex>
 #include <exception>
 
+// 핵심 : mutex 를 사용할때 되도록이면 직접
+//       lock()/unlock() 하지 마세요.
+//       lock 관리도구(4개)를 사용하세요.
+
+// => 직접 lock 하면.. 예외 등이 발생시
+//    "dead lock" 이 될수 있습니다.
+
 std::mutex m;
 
 void goo()
