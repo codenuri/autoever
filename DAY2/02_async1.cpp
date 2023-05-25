e0aafeb3d490ba5a32ca70f3bdda2d387cc496fe
ecourse <smkang@codenuri.co.kr>
2023-05-25 09:24:53 +0900

    Add files via upload

--- DAY2/02_async1.cpp
+++ DAY2/02_async1.cpp
@@ -3,6 +3,19 @@
 #include <future>
 #include <chrono>
 using namespace std::literals;
+
+// async 특징
+// 1. 사용자가 "std::thread" 를 사용할 필요 없이
+//    std::async() 내부적으로 스레드 생성
+//    정확히는 "스레드 풀" 사용
+
+// 2. 스레드를 고려하지 않은 함수(return 을 결과를 반환하는 함수)
+//    를 스레드로 수행해서 결과를 얻을수 있다.
+//    std::future<> 로 결과 얻기
+
+
+
+
 // 스레드를 고려하지 않은 일반 함수라면 아래 처럼 작성합니다.
 int add(int a, int b)
 {
