b0de27a0efc84debb70d841b4fac8f65d9273ef7
ecourse <smkang@codenuri.co.kr>
2023-05-25 10:20:15 +0900

    Add files via upload

--- DAY2/02_async5.cpp
+++ DAY2/02_async5.cpp
@@ -44,14 +44,57 @@ long long multi_thread()
     auto v2_first = v2.begin();
     auto v1_last = std::next(v1.begin(), block_size);
 
+    // std::thread : 결과를 얻기 위해 promise/future 등의방법을 사용해야 합니다.
+    // std::async  : 기존 함수의 반환값을 그대로 사용가능
 
+    std::future<long long> ft1 = std::async( 
+        [v1_first, v1_last, v2_first]()
+        { 
+            return std::inner_product(v1_first, v1_last, v2_first, 0LL);
+        } 
+    );
+
+    std::advance(v1_first, block_size);
+    std::advance(v2_first, block_size);
+    std::advance(v1_last,  block_size);
+
+    std::future<long long> ft2 = std::async(
+        [v1_first, v1_last, v2_first]()
+        {
+            return std::inner_product(v1_first, v1_last, v2_first, 0LL);
+        }
+    );
+
+    std::advance(v1_first, block_size);
+    std::advance(v2_first, block_size);
+    std::advance(v1_last, block_size);
+
+    std::future<long long> ft3 = std::async(
+        [v1_first, v1_last, v2_first]()
+        {
+            return std::inner_product(v1_first, v1_last, v2_first, 0LL);
+        }
+    );
+
+    std::advance(v1_first, block_size);
+    std::advance(v2_first, block_size);
+    v1_last = v1.end();
+
+    std::future<long long> ft4 = std::async(
+        [v1_first, v1_last, v2_first]()
+        {
+            return std::inner_product(v1_first, v1_last, v2_first, 0LL);
+        }
+    );
+
+    // 이제 주스레드가 각각 스레드의 결과를 대기했다가 더하면 됩니다.
+    return ft1.get() + ft2.get() + ft3.get() + ft4.get();
 }
-
-
 
 int main()
 {
     fill_vector();
     
     single_thread();
+    multi_thread();
 }
