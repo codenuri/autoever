37c68a27d879a7fb93b623743aeef9695165caf6
ecourse <smkang@codenuri.co.kr>
2023-05-25 16:25:29 +0900

    Add files via upload

---
+++ DAY2/threadpool5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#include <future>
+
+// 스레드를 고려하지 않은 일반 함수라면 아래 처럼 작성합니다.
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+	// 1. 함수를 직접 호출하면 즉시 반환값을 받을수 있습니다.
+	int ret = add(1, 2);
+
+
+	// 2. 함수를 packaged_task 로 묶으면 호출하지 않아도
+	//    결과를 받을수 있습니다.
+	std::packaged_task<int(int, int)> task(add);
+
+	std::future<int> ft = task.get_future(); //호출하기 전에 결과받기
+	task(1, 2);
+
+}
+
