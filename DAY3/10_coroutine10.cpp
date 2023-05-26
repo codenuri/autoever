6f11d22d5e87df508cb27719e128c231aae0b5e3
ecourse <smkang@codenuri.co.kr>
2023-05-26 16:09:10 +0900

    Add files via upload

---
+++ DAY3/10_coroutine10.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <coroutine>
+
+// Step 9. std::suspend_always
+
+class Generator
+{
+public:
+	class promise
+	{
+		int value;
+	public:
+		int get_value() const { return value; }
+
+		std::suspend_always yield_value(int n)
+		{
+			value = n;
+			return  {}; // suspend_always 타입의 객체 반환
+		}
+
+		std::suspend_always initial_suspend() { return {}; }
+		std::suspend_always final_suspend() noexcept { return {}; }
+
+		Generator get_return_object()
+		{
+			Generator g;
+			g.coro = std::coroutine_handle<promise_type>::
+				from_promise(*this);
+
+			return g;
+		};
+
+		void unhandled_exception() {}
+		void return_void() {}
+	};
+	using promise_type = promise;
+
+	std::coroutine_handle<promise_type> coro;
+
+	~Generator()
+	{
+		if (coro) coro.destroy(); // 코루틴 프레임 워크 파괴
+	}
+
+	int  operator()() const
+	{
+		coro.resume(); // 코루틴 재개하고
+
+		int ret = coro.promise().get_value();
+
+		return ret; // co_yield 반환값 반환
+	}
+};
+
+
+
+Generator foo()
+{
+	std::cout << "foo 1" << std::endl;
+	
+	co_await std::suspend_always{};
+//	co_await std::suspend_never{};
+
+	std::cout << "foo 2" << std::endl;
+}
+
+int main()
+{
+	Generator g = foo();
+
+	g();
+
+
+}
