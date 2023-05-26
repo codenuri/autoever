b6a0ff0d9477ffc468896790d636503bea688110
ecourse <smkang@codenuri.co.kr>
2023-05-26 10:40:46 +0900

    Add files via upload

--- DAY3/04_atomic5.cpp
+++ DAY3/04_atomic5.cpp
@@ -15,6 +15,10 @@ struct Point
 
 int main()
 {
+    Point pt; // 디폴트 생성자가 trivial 하면 호출 안함.
+              // 디폴트 생성자가 trivial 하지 않으면 호출..
+              // => 단, 최적화 단계에서 생략될수도 있음.
+
     std::cout << std::is_trivially_copy_constructible_v<Point> << std::endl;
 
     std::cout << std::is_trivially_default_constructible_v<Point> << std::endl;
