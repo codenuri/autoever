6d530870b875a42fab9c4b680e3ee2b43fbb92c2
ecourse <smkang@codenuri.co.kr>
2024-06-17 13:14:51 +0900

    Add files via upload

--- DAY1/3_가상함수2.cpp
+++ DAY1/3_가상함수2.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
+// 핵심
+// 기반 클래스의 소멸자는 반드시 "virtual" 이어야 한다. "가상소멸자 문법"
+// 단, virtual 로 하지 않으려면 "protected"에 놓아야 한다.
+
 class Base
 {
 public:
-	~Base() {}
+//	~Base() {}
+	virtual ~Base() {}	// 가상 소멸자
 };
 class Derived : public Base
 {
@@ -23,4 +28,18 @@ int main()
 	Base* p = new Derived;
 	delete p;
 
+	// 
+	// "delete p" 에서 소멸자를 호출해야 합니다.
+	// "소멸자 호출" 도 결국 "함수의 호출" 입니다.
+
+	// 1. p 의 타입이 Base* 이므로 Base 클래스를 조사해서
+	//    소멸자가 virtual 인지 아닌지를 먼저 조사합니다.
+
+	// 2. 소멸자가 virtual     : dynamic binding
+	//							=> 실행시간에 p가 가리키는 메모리 조사후
+	//							=> 소멸자 결정
+	// 
+	// 3. 소멸자가 non-virtual : static  binding
+	//							=> 포인터 타입으로 소멸자 결정
+	//							=> p가 Base* 이므로 ~Base() 만 호출
 }	
