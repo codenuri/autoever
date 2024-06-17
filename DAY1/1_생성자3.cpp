7f9b312a165fbc28f550528fd01ddd477a3ffd8d
ecourse <smkang@codenuri.co.kr>
2024-06-17 09:49:41 +0900

    Add files via upload

--- DAY1/1_생성자3.cpp
+++ DAY1/1_생성자3.cpp
@@ -10,6 +10,17 @@ public:
 	Person(const std::string& name, int age) : name(name), age(age) {}
 };
 
+// 1. Person 으로 부터 파생된 "Student" 클래스 만들어 보세요
+// 2. "int id" 멤버 추가하세요
+// 3. Student 에 생성자 제공해서 id 초기화 하세요
+// 4. main 에서 Student 객체 생성해 보세요
+
+class Student : public Person
+{
+
+};
+
+
 int main()
 {
 	Person p1("kim", 20); // ok. 초기화된 객체. 안전하다.
