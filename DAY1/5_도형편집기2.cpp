c70da7a042bbdb4478d30412afb5e3f786e14afa
ecourse <smkang@codenuri.co.kr>
2024-06-17 15:08:19 +0900

    Add files via upload

--- DAY1/5_도형편집기2.cpp
+++ DAY1/5_도형편집기2.cpp
@@ -34,11 +34,15 @@ int main()
 		else if (cmd == 2) v.push_back(new Circle);
 		else if (cmd == 9)
 		{
-			for (auto p : v)
-				v->draw();
+			for (auto p : v)	// A
+				p->draw();		// B. error
 		}
 	}
 }
 // #1. 위 코드는 왜 에러 일까요 ?
-
+// => A 부분에서 "auto p" 은 결국 "Shape* p" 입니다.
+// => 그런데, Shape 에는 draw() 가 없습니다.
+// => 그래서 "B" 부분에서 에러!!
+// 
 // #2. 해결책은 뭘까요 ?
+// => 다음 소스에서!!(Shape3.cpp, Shape4.cpp)
