aa65088feb2a635290bedab260e6578fea4306d1
ecourse <smkang@codenuri.co.kr>
2024-06-17 09:56:44 +0900

    Add files via upload

--- DAY1/1_생성자3.cpp
+++ DAY1/1_생성자3.cpp
@@ -17,12 +17,24 @@ public:
 
 class Student : public Person
 {
+	int id;
+public:
+	// 아래 처럼 만들면 오른쪽 주석 처럼 컴파일러가 추가한 코드 때문에 에러
+	// => Person 에 디폴트 생성자가 없습니다.
+//	Student(int id) : id(id) {} // Student(int id) : Person(), id(id) {}
 
+	// 해결책
+	// => Student 제작자는 Person 에 디폴트 생성자가 없다는 것을 알아야 합니다.
+	// => Student 생성자에서 Person 의 다른 생성자를 명시적으로 호출해야합니다.
+	
+	Student(const std::string& name, int age, int id) 
+		: Person( name, age ),  id(id) {}
 };
-
 
 int main()
 {
+	Student s("kim", 20, 15);
+
 	Person p1("kim", 20); // ok. 초기화된 객체. 안전하다.
 
 //	Person p1;	// 이렇게 객체를 만들수 있다는 것이 좋을까요 ? (초기화 되지 않은 객체)
