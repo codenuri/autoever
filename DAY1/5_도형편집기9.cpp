683fdf13f1a7329364a53d885d1048198e1176d1
ecourse <smkang@codenuri.co.kr>
2024-06-17 16:41:58 +0900

    Add files via upload

--- DAY1/5_도형편집기9.cpp
+++ DAY1/5_도형편집기9.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 
-
+class NotImplemented {}; // 예외를 위한 클래스
 
 
 class Shape
@@ -21,37 +21,48 @@ public:
 		std::cout << "mutex unlock\n";
 	}
 
+	// 가상함수      : 파생 클래스가 override 하지 않으면 기본 구현을 제공하겠다는 의미
+	// 순수 가상함수 : 파생 클래스가 반드시 구현을 제공하라고 지시하는 것. 
+
 protected:
-	virtual void draw_imp()
-	{
-		std::cout << "draw Shape\n";
-	}
+	virtual void draw_imp() = 0;
 
 public:
-	virtual Shape* clone() const { return new Shape(*this); }
 
-	virtual int get_area() const { return 0; }
+//	virtual Shape* clone() const = 0; // 이렇게 해도 되고!! 좋은 코드!!
+
+	// 아래 같이 만드는 경우도 있습니다.
+	// 1. override 하지 않고 사용하면 예외 발생
+	// 2. override 하지 않고 사용하지도 않으면 ok
+	// 3. override 하고 사용하면 ok. 
+	// => 즉, 사용하는 경우만 override 하면 된다는 것. "파이썬 등에서 많이 사용"
+
+	virtual Shape* clone() const { throw NotImplemented(); }
+
+
+	// 아래 코드도 "=0", "기본 구현 예외"로 해도 되고
+	// => 실패를 나타내는 값을 반환하게 해도 됩니다.
+	// => "get_area() 호출시 -1이 나오면 면적구할수 없음" 이라고 매뉴얼작성.
+	virtual int get_area() const { return -1; }
 
 };
-
-
 
 
 class Rect : public Shape
 {
 public:
-	void draw_imp() { std::cout << "draw Rect\n"; }
+	void draw_imp() override { std::cout << "draw Rect\n"; }
 
-	virtual Shape* clone() const { return new Rect(*this); }
+	Shape* clone() const override  { return new Rect(*this); }
 };
 
 
 class Circle : public Shape
 {
 public:
-	void draw_imp() { std::cout << "draw Circle\n"; }
+	void draw_imp() override { std::cout << "draw Circle\n"; }
 
-	virtual Shape* clone() const { return new Circle(*this); }
+	Shape* clone() const override { return new Circle(*this); }
 };
 
 
