9cd108f7e2d636fdbbe22f08b968af59dd97f060
ecourse <smkang@codenuri.co.kr>
2024-06-17 10:45:56 +0900

    Add files via upload

--- DAY1/2_upcasting1-1.cpp
+++ DAY1/2_upcasting1-1.cpp
@@ -25,13 +25,18 @@ int main()
 	// static_cast  : 컴파일 시간 캐스팅
 	// dynamic_cast : 실행   시간 캐스팅
 
-	// static_cast : 컴파일 시간에 캐스팅
+	// #1. static_cast : 컴파일 시간에 캐스팅
 	// 장점 : 오버헤드가 전혀 없다. 빠르다.
 	// 단점 : 객체를 조사할수 없다.
 
 	p = &a; // p는 Animal 객체를 가리킨다.
+
 	Dog* pdog = static_cast<Dog*>(p);	// 잘못된 캐스팅이지만
 										// 조사할수 없으므로 그냥 주소가 나온다
+
+	std::cout << pdog << std::endl; // 주소가 나온다!!
+									// 단, 이주소는 사용하면 안됨..	
+									// pdog 가 가리키는 곳은 Dog 가 아닌 Animal
 }
 
 
