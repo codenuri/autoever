687fad4b24ada699bf324419541301cf3f60f4b5
ecourse <smkang@codenuri.co.kr>
2024-06-17 15:04:02 +0900

    Add files via upload

--- DAY1/5_도형편집기2.cpp
+++ DAY1/5_도형편집기2.cpp
@@ -23,5 +23,22 @@ public:
 int main()
 {
 	std::vector<Shape*> v;	
-}
 
+	while (1)
+	{
+		int cmd;
+
+		std::cin >> cmd;
+
+		if      (cmd == 1) v.push_back(new Rect);
+		else if (cmd == 2) v.push_back(new Circle);
+		else if (cmd == 9)
+		{
+			for (auto p : v)
+				v->draw();
+		}
+	}
+}
+// #1. 위 코드는 왜 에러 일까요 ?
+
+// #2. 해결책은 뭘까요 ?
