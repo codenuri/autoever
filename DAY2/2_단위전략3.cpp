e7dcfe8803393f0e4c9007a8fb11758d48c56d84
ecourse <smkang@codenuri.co.kr>
2024-06-18 11:17:53 +0900

    Add files via upload

--- DAY2/2_단위전략3.cpp
+++ DAY2/2_단위전략3.cpp
@@ -50,3 +50,10 @@ int main()
 	MallocAllocator<int> m;
 	vector<int> v(10, &m); 
 }
+// 위 방식의 특징
+// 장점 : 메모리 할당 정책이 컨테이너(vector)와 분리되어 있다.
+//        vector 뿐 아니라 다른 컨테이너(list, set 등)도 MallocAllocator 사용가능
+// 
+// 단점 : allocate/deallocate 가 "가상함수" 이다. 호출시 느리다
+//        가상함수 테이블 때문에 메모리 오버헤드가 있다.
+//        STL 같은 표준 라이브러리는 "절대 오버헤드"가 있으면 안된다.
