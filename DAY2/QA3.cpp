9a40fc0e56c245ae3fbb4f23595cd49180c54d43
ecourse <smkang@codenuri.co.kr>
2024-06-18 16:24:38 +0900

    Add files via upload

--- DAY2/QA3.cpp
+++ DAY2/QA3.cpp
@@ -12,6 +12,12 @@ int main()
 		if (std::cin.fail())
 		{
 			std::cout << "입력 실패\n";
+
+			// 다시 시도하기 전에..
+			// 아래 2개의 작업을 해야 합니다.(반드시 순서를 지켜야 합니다.)
+			std::cin.clear();  // 1. cin 의 상태 비트를 모두 clear 해야합니다.
+			std::cin.ignore(); // 2. 입력 버퍼를 비워야 합니다.
+			
 			continue;
 		}
 
