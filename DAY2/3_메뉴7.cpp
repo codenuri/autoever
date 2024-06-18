59478da69b387da8f8c7375dd371e46324d0edc3
ecourse <smkang@codenuri.co.kr>
2024-06-18 14:47:08 +0900

    Add files via upload

---
+++ DAY2/3_메뉴7.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h> 
+
+class BaseMenu
+{
+	std::string title;
+public:
+	BaseMenu(const std::string& title) : title(title) {}
+
+	virtual ~BaseMenu() {}
+
+	const std::string& get_title() const { return title; }
+
+	virtual void command() = 0;
+};
+
+
+
+
+
+
+class MenuItem : public BaseMenu
+{
+	int id;
+public:
+	MenuItem(const std::string& title, int id) : BaseMenu(title), id(id) {}
+
+	void command()
+	{
+		std::cout << get_title() << " 메뉴 선택\n";
+		_getch();
+	}
+};
+
+class PopupMenu : public BaseMenu
+{
+	std::vector<BaseMenu*> v;
+public:
+
+	PopupMenu(const std::string& title) : BaseMenu(title) {}
+
+	void add(BaseMenu* m) { v.push_back(m); }
+
+
+
+	void command()
+	{
+		while (1)
+		{
+			system("cls");
+
+			auto sz = v.size();
+
+			for (int i = 0; i < sz; i++)
+			{
+				std::cout << i + 1 << ". " << v[i]->get_title() << '\n';
+			}
+			std::cout << sz + 1 << ". 종료\n";
+			std::cout << "메뉴를 선택하세요 >> ";
+
+			int cmd;
+			std::cin >> cmd;
+
+			if (cmd == sz + 1)
+				break;
+
+			if (cmd < 1 || cmd > sz + 1)
+				continue;
+
+
+			v[cmd - 1]->command();
+		}
+	}
+};
+
+
+
+
+
+
+
+
+
+int main()
+{
+	PopupMenu* root = new PopupMenu("ROOT");
+	
+	root->add(new PopupMenu("확대/축소")    );
+	root->add(new MenuItem("상태표시줄", 11));
+
+
+	// 아래 코드에 대해서 생각해 봅시다.
+	root->get_submenu(0)->add( new MenuItem("확대", 21) );
+
+
+	root->command();
+}
+
+
+
