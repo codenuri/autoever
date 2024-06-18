c9d28d556e72eb29dcb86a12ea290b1423865ceb
ecourse <smkang@codenuri.co.kr>
2024-06-18 13:26:48 +0900

    Add files via upload

---
+++ DAY2/3_메뉴2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h> 
+
+// 객체지향 프로그래밍 
+// => 모든 것은 객체이다.!
+// => 필요한 타입을 먼저 설계 한다.
+
+
+
+int main()
+{
+	MenuItem m1("김밥", 11);
+	MenuItem m2("라면", 12);
+
+	m1.command(); // 메뉴 선택시 호출된다고 가정. 
+}
+
+
+
+
