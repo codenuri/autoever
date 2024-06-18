c6f2418651b3bd53c9f9bfe1ec351c1cb0c5a5bb
ecourse <smkang@codenuri.co.kr>
2024-06-18 13:31:21 +0900

    Add files via upload

--- DAY2/3_메뉴2.cpp
+++ DAY2/3_메뉴2.cpp
@@ -7,6 +7,23 @@
 // => 모든 것은 객체이다.!
 // => 필요한 타입을 먼저 설계 한다.
 
+// 메뉴항목 한개를 나타내는 클래스
+class MenuItem
+{
+	std::string title;
+	int id;
+public:
+	MenuItem(const std::string& title, int id) : title(title), id(id) {}
+
+	const std::string& get_title() const { return title; }
+
+	// 메뉴 선택시 호출되는 함수
+	void command()
+	{
+		std::cout << get_title() << " 메뉴 선택\n";
+		_getch();
+	}
+};
 
 
 int main()
