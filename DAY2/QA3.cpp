2e9f380128c7a46b411bc0ca2b551d39d089996b
ecourse <smkang@codenuri.co.kr>
2024-06-18 16:18:31 +0900

    Add files via upload

--- DAY2/QA3.cpp
+++ DAY2/QA3.cpp
@@ -5,11 +5,17 @@ int main()
 {
 	int n = 0;
 
-	std::cin >> n;	// 'a' 입력하세요
-	
-	if (std::cin.fail())
+	while (1)
 	{
-		std::cout << "입력 실패\n";
+		std::cin >> n;	// 'a' 입력하세요
+
+		if (std::cin.fail())
+		{
+			std::cout << "입력 실패\n";
+			continue;
+		}
+
+		break;
 	}
 
 	std::cout << n << std::endl; 
