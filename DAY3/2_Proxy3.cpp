7a9ca359ca1b0845f00d562e3001939b373d3e17
ecourse <smkang@codenuri.co.kr>
2024-06-19 14:19:29 +0900

    Add files via upload

--- DAY3/2_Proxy3.cpp
+++ DAY3/2_Proxy3.cpp
@@ -20,7 +20,8 @@ int main()
 	// Client 가 아래처럼 Proxy 이름을 직접 사용하면 "강한 결합" 입니다.
 	// Proxy 가 Update 되면 아래 코드는 수정되어야 합니다.
 	// Client 는 Proxy 사용시 "약한 결합" 으로 사용해야 합니다.
-	Calc* calc = new Calc;
+	Calc* calc = new Calc; // 나쁜 코드
+							// 해결은 Proxy4.cpp
 
 	int n1 = calc->Add(1, 2);
 	int n2 = calc->Sub(1, 2);
