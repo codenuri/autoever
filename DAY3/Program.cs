2b9a189edce8aae37177bb46ca47a79894cb46ca
ecourse <smkang@codenuri.co.kr>
2024-06-19 10:50:12 +0900

    Add files via upload

--- DAY3/Program.cs
+++ DAY3/Program.cs
@@ -1,9 +1,20 @@
 ﻿
+using System.IO.Compression;
+
 byte[] data = new byte[1024 * 1024]; // 1M 크기의 데이타
 
 FileStream fs = new FileStream("a.dat", FileMode.Create);
 
-fs.Write(data); // 데이타는 그냥 파일에 쓰기. 파일 크기가 1M 가 됩니다.
+//fs.Write(data); // 데이타는 그냥 파일에 쓰기. 파일 크기가 1M 가 됩니다.
+
+
+GZipStream gs = new GZipStream(fs, CompressionLevel.Optimal);
+
+gs.Write(data); // 1. 압축기능 수행후 
+                // 2. fs.Write 로 쓰기
+
 
 fs.Close();
 
+// 실행후 "a.dat" 파일의 크기를 확인하세요
+// bin/debug/net8.0/a.dat 있습니다.
