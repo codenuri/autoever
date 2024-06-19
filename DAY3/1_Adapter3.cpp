981cb720c8d1e318b0d460b475d2dd2351247aa3
ecourse <smkang@codenuri.co.kr>
2024-06-19 13:07:32 +0900

    Add files via upload

--- DAY3/1_Adapter3.cpp
+++ DAY3/1_Adapter3.cpp
@@ -15,12 +15,17 @@ public:
 	// 결국 아래 3개 함수는 "list 의 함수 이름을 stack 처럼 보이게 변경"한것
 	void push(const T& e) { std::list<T>::push_back( e ); }
 	void pop()            { std::list<T>::pop_back(); }
+
 	T&   top()            { return std::list<T>::back();}
 };
-
 
 int main()
 {
 	stack<int> s;
 	s.push(10);
+
+	// 위 구현의 문제점.. 
+	// stack 사용자가 아래 처럼 사용할수 있게 됩니다.
+	s.push_front(20); // !!!!! 뒤로만 사용해야 하는데.. 앞으로 접근..
+					  // 해결책은 다음소스
 }
