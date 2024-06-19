ed5c2c462541885d35be32e8c195cbb995fd1d8d
ecourse <smkang@codenuri.co.kr>
2024-06-19 14:08:37 +0900

    Add files via upload

--- DAY3/2_Proxy2.cpp
+++ DAY3/2_Proxy2.cpp
@@ -6,6 +6,7 @@
 // 1. 모든 클라이언트 제작자가 IPC 기술을 알아야 한다.
 // 2. 서버접속에 대한 장애처리를 모든 클라이언트가 각각 해야 한다.
 
+
 // Proxy        : 대행자를 만드는 패턴
 // remote-proxy : 원격지 서버에 대한 대행자
 
@@ -13,32 +14,20 @@ class Calc
 {
 	int server;
 public:
-	Calc() { }
+	Calc() { server = ec_find_server("Calc"); }
 
-	int Add(int a, int b) { return ? ; }
-	int Sub(int a, int b) { return ? ; }
+	int Add(int a, int b) { return ec_send_server(server, 1, a, b); }
+	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); }
 };
-
-
-
-
-
-
-
 
 int main()
 {
-	int server = ec_find_server("Calc");
+	Calc* calc = new Calc;
 
-	std::cout << "서버 번호 : " << server << std::endl;
-
-
-	
-	int n1 = ec_send_server(server, 1, 10, 20);
-	int n2 = ec_send_server(server, 2, 10, 20);
+	int n1 = calc->Add(1, 2);
+	int n2 = calc->Sub(1, 2);
 
 	std::cout << n1 << ", " << n2 << std::endl;
-
 }
 
 
