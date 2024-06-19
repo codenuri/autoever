77ac2aa475c13f8749e2d8d77f1c1b58275fe0f3
ecourse <smkang@codenuri.co.kr>
2024-06-19 16:11:22 +0900

    Add files via upload

--- DAY3/4_Bridge1.cpp
+++ DAY3/4_Bridge1.cpp
@@ -18,11 +18,17 @@ public:
 class People
 {
 public:
-//	void use(IMP3* p)
-	void use(IPod* p)
+	void use(IMP3* p)	// 약한 결합 : 다양한 MP3 제품 사용가능
+//	void use(IPod* p)	// 강한 결합 : 한가지 제품(IPod)만 사용가능. 경직된 디자인
 	{
-		p->Play();
-		p->Stop();
+		p->play();
+		p->stop();
+
+		// 그런데, "1분 미리듣기" 기능이 필요하다.
+		p->play_one_minute();
+
+		// 요구되는 기능을 제공하려면 "인터페이스(IMP3)" 를 수정해야 한다.
+		// => 인터페이스를 변경하는 것은 기존의 모든 제품을 수정해야 한다.
 	}
 };
 
