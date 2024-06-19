1dd59b0919df59da861c5f27d04f8c7fbbb59712
ecourse <smkang@codenuri.co.kr>
2024-06-19 10:38:23 +0900

    Add files via upload

--- DAY3/0_Decorator8.cpp
+++ DAY3/0_Decorator8.cpp
@@ -66,3 +66,9 @@ int main()
 	ZipDecorator zd(&ed);
 	zd.write("data");	
 }
+
+// 위 처럼 설계 했다면 "Stream" 관련 기능이 필요할때마다
+// 기능 추가 클래스(Decorator) 들만 계속 만들면 됩니다.
+// 하나의 Decorator 로 모든 Stream 에도 적용 가능합니다.!!
+
+// 구글에서 "C# Stream" 검색해 보세요. (이미지 보기로 해보세요)
