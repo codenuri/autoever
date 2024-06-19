e87795591f7321b9b7a504011bb15edc2e750111
ecourse <smkang@codenuri.co.kr>
2024-06-19 10:45:45 +0900

    Add files via upload

---
+++ DAY3/Program.cs
@@ -0,0 +1,9 @@
+﻿
+byte[] data = new byte[1024 * 1024]; // 1M 크기의 데이타
+
+FileStream fs = new FileStream("a.dat", FileMode.Create);
+
+fs.Write(data); // 데이타는 그냥 파일에 쓰기. 파일 크기가 1M 가 됩니다.
+
+fs.Close();
+
