06c64d8de05426c58ecda00591bacd0f1fc19e62
ecourse <smkang@codenuri.co.kr>
2024-06-20 16:12:58 +0900

    Add files via upload

---
+++ DAY4/7_Command1.cpp
@@ -0,0 +1,95 @@
+// 8_Command - 172 page
+#include <iostream>
+#include <vector>
+#include <stack>
+
+class Shape
+{
+public:
+	virtual void draw() = 0;
+	virtual ~Shape() {}
+};
+
+
+class Rect : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Rect" << std::endl; }
+};
+class Circle : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Circle" << std::endl; }
+};
+
+// 메뉴를 선택시 어떤일을 해야 한다면
+// 
+// C 언어 : 해야할 일을 함수로 작성합니다.
+//			=> 해야할 일만 수행합니다.
+// 
+// Command 패턴 : 해야할 일을 클래스로 작성 합니다.
+//			=> 해야할일과 
+//			=> 취소할때 동작
+//			=> 취소에 필요한 데이타등을 모두 보관 가능합니다.
+//			=> "작업을했다는 사실을 자료구조에 보관"도 가능합니다.
+
+// 모든 명령의 인터페이스
+struct ICommand
+{
+	virtual void execute() = 0;
+	virtual bool can_undo() { return false; }
+	virtual void undo() {}
+	virtual ~ICommand() {}
+};
+
+// 사각형을 추가하는 명령
+class AddRectCommand : public ICommand
+{
+	std::vector<Shape*>& v;
+public:
+	AddRectCommand(std::vector<Shape*>& v) : v(v) {}
+
+	void execute() override { v.push_back(new Rect); }
+
+	bool can_undo() override { return true; }
+
+	void undo() override
+	{
+		Shape* s = v.back();
+		v.pop_back();
+		delete s;
+	}
+};
+
+
+
+
+
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd == 1) 
+		{
+			v.push_back(new Rect);
+		}
+		else if (cmd == 2) 
+		{
+			v.push_back(new Circle);
+		}
+		else if (cmd == 9)
+		{
+			for (auto p : v)
+				p->draw();
+		}
+
+	}
+}
+
+
