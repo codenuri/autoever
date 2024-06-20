387560c2d9b7323d2d11a37461d6f03341682c66
ecourse <smkang@codenuri.co.kr>
2024-06-20 14:38:50 +0900

    Add files via upload

--- DAY4/QA2.cpp
+++ DAY4/QA2.cpp
@@ -14,4 +14,11 @@ public:
 int main()
 {
 	A<int> a;
+	a.foo(); // 이렇게 사용하는 경우에만 "template 인스턴스화에 포함됩니다."
 }
+// 지연된 인스턴스화
+// "class template => 실제 class" 를 생성할때
+// 사용된 멤버만 인스턴스화에 포함됩니다.
+// 위 코드에서 "a.foo()" 가 없다면 "실제 생성된 class" 에 "foo"가 포함안됩니다
+
+
