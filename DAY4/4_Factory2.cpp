51ee322d6bff5989c7cae7d1c6c320fc9fd523cc
ecourse <smkang@codenuri.co.kr>
2024-06-20 13:24:28 +0900

    Add files via upload

--- DAY4/4_Factory2.cpp
+++ DAY4/4_Factory2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include "Helper.h"
 
 class Shape
@@ -24,31 +25,51 @@ public:
 // 1. Rect* r1 = new Rect;			// 클래스 이름을 직접 사용
 // 2. Rect* r2 = Rect::create();	// 객체 생성 함수 호출
 
+// 1. C++ 언어는 "클래스" 를 자료구조에 보관할수 없습니다
+// v.push_back("Rect");  // 클래스를 보관한 것이 아니라 문자열 "Rect" 보관
+	    				 // 문자열 "Rect" 로는 "Rect 객체 생성" 안됩니다.
 
+// 2. 하지만 함수 포인터는 자료 구조에 보관 가능합니다
+//    즉, 생성함수를 자료구조에 보관하면 나중에 보관된 정보로 객체 생성가능합니다
+// v.push_back( &Rect::create );
 
-
-
-
-
-
+// 이 외에도 "생성함수주소" 는 함수의 인자 등으로도 전달가능합니다.
 
 
 class Circle : public Shape
 {
 public:
 	void draw() override { std::cout << "draw Circle" << std::endl; }
-};
 
+	static Shape* create() { return new Circle; }
+}; 
 
 class ShapeFactory
 {
 	MAKE_SINGLETON(ShapeFactory)
+
+	using CREATOR = Shape * (*)(); // 함수 포인터 타입
+
+	std::map<int, CREATOR> create_map; // <도형key, 생성함수> 를 자료구조에 보관
+
 public:
+	void register_shape(int key, CREATOR c)
+	{
+		create_map[key] = c;
+	}
+
 	Shape* create(int type)
 	{
 		Shape* p = nullptr;
-		if (type == 1)	p = new Rect;
-		else if (type == 2)	p = new Circle;
+		
+		auto it = create_map.find(type);
+
+		if (it != create_map.end())
+		{
+			p = it->second();   // 반복자 it 는 pair 의 포인터 역활 입니다
+								// it->first : key
+								// it->second : value 즉, 함수 포인터!
+		}
 		return p;
 	}
 };
