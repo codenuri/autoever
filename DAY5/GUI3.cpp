a8fc76a503348daa52491995692b07af404d40ec
ecourse <smkang@codenuri.co.kr>
2024-06-21 15:53:39 +0900

    Add files via upload

--- DAY5/GUI3.cpp
+++ DAY5/GUI3.cpp
@@ -1,24 +1,40 @@
 #define USING_GUI
 #include "cppmaster.h"
-
+#include <map>
 
 
 // GUI 라이브러리를 C++로 만들어 봅시다.
 class Window
 {
 	int handle;
+	
+	inline static std::map<int, Window*> this_map;
+				// <윈도우 번호, this> 를 보관하기 위한 자료구조
+
 public:
 	void create(const std::string& title)
 	{
 		handle = ec_make_window(msgproc, title);
+
+		this_map[handle] = this;
 	}
 
 	static int msgproc(int hwnd, int msg, int a, int b)
 	{
+		Window* self = this_map[hwnd];
+		
+		// 이제 self 를 this 처럼 생각하고 사용하면됩니다.
+		// 그러면 static 멤버 함수에서도 모든 멤버 접근 가능합니다.
 		switch (msg)
 		{
-		case WM_LBUTTONDOWN: lbutton_down() ; break;
-		case WM_KEYDOWN:     key_down(); break;
+		case WM_LBUTTONDOWN: 
+			//lbutton_down();		// this->lbutton_down();
+			self->lbutton_down();
+			break;
+
+		case WM_KEYDOWN:     
+			self->key_down(); 
+			break;
 		}
 		return 0;
 	}
