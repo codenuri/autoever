4d44e41c5a4959893e1c596536e0b6ef05763de9
ecourse <smkang@codenuri.co.kr>
2024-06-21 16:29:11 +0900

    Add files via upload

---
+++ DAY5/정리.cpp
@@ -0,0 +1,32 @@
+// 정리
+
+// 디자인패턴 23개는 용도에 따라 아래 3가지로 구분됩니다.
+// => 교재 마지막 페이지 참고
+
+// 생성 패턴 5개 : 객체를 만들때 사용하는 패턴.
+// 
+// singleton : 오직 한개의 객체만
+// prototype : clone !!
+// abstract factory
+// factory method
+// builder 
+
+
+// 구조 패턴 7개
+// => 재귀적 포함을 사용하는 2개 : composite, decorator
+// => 간접층을 사용하는 4개     :  adapter, bridge, facade, proxy
+// => 속성이 동일한 것은 공유   : flyweight
+
+
+// 행위 패턴 11개
+// 변하는 것의 분리 : template method, strategy, state 
+// 열거, 방문      : iterator, visitor
+
+// 통보, 전달      : observer, chain of responsibility  
+
+// 객체상태를  저장/복구 : memento
+// 명령을 클래스로 설계 : command ( undo/redo) 
+
+// 객체간의 관계가 복잡하면 중재자를 만들어라 : mediator
+
+// 컴파일러의 파서를 만들때 사용하는 패턴(지금은 거의 사용안함) : interpreter
