843aade2e7312ba3ce8e88a966eb6d5595275a75
ecourse <smkang@codenuri.co.kr>
2026-08-31 15:39:17 +0900

    Add files via upload

---
+++ DAY1/05_control_statement2_switch.cs
@@ -0,0 +1,26 @@
+using System;
+
+// 핵심 : switch 문
+int num = 1;
+
+// switch : 값에 따른 분기문 작성할때 사용
+
+switch( num  )
+{
+    case 1: 
+        Console.WriteLine("one");
+        break;
+    case 2:
+        Console.WriteLine("two");
+        break;
+
+    default:
+        Console.WriteLine("other");
+        break;
+}
+
+
+
+object obj = 3.4;
+
+
