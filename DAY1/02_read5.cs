15ceaaf1c846ee49de54bef19a2970c619875a27
ecourse <smkang@codenuri.co.kr>
2026-08-31 12:42:14 +0900

    Add files via upload

---
+++ DAY1/02_read5.cs
@@ -0,0 +1,13 @@
+﻿
+// Console.ReadLine()
+// Console.Read()
+// 
+// 위 2개는 결국 입력 버퍼에서 가지고 오는 것
+// => 입력 버퍼가 비었을때는 "사용자-> 입력 버퍼"에 담아야 하고
+// => enter 를 해야 종료
+
+ConsoleKeyInfo key = Console.ReadKey();
+
+Console.WriteLine("입력된 키보드 {0}", key.KeyChar);
+
+
