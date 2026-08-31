39055ee7d437bffe8dda0d521978db0233cc8985
ecourse <smkang@codenuri.co.kr>
2026-08-31 16:28:09 +0900

    Add files via upload

--- DAY1/06_loop4_foreach.cs
+++ DAY1/06_loop4_foreach.cs
@@ -9,8 +9,14 @@ for (int i = 0; i < 10; i++)
 }
 
 // #2. C# 에서 만든 C 에는 없던 반복문
-
+//       for e in x    <= 파이썬과 동일한 모양
 foreach( int e in x )
 {
     WriteLine($"{e}")
 }
+// C#     : while, do-while, for, foreach
+// Python : while     X       X   for 
+// C++    : while  do-while  for  for 
+// Rust   : while,    X       X   for
+
+// 앞으로 예제에서는 조건문과 반복문은 모두 알고 있다는 가정하에 수업
