62e7d9c081987f197195c81258b1e147a3244247
ecourse <smkang@codenuri.co.kr>
2026-08-31 11:36:47 +0900

    Add files via upload

---
+++ DAY1/01_using1.cs
@@ -0,0 +1,10 @@
+
+// 아래 코드에서
+
+// System  : Namespace 문법 ( Python 은 모듈 개념)
+// Console : 클래스 이름
+// WriteLine : 정적 메소드(함수)이름
+
+System.Console.Write("Hello, ");    // 출력후 개행 안함(print)
+System.Console.WriteLine("C#");     // 출력후 개행     (println)
+
