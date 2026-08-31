ef0f6194141544d15de793fdae9c9e26026dd09d
ecourse <smkang@codenuri.co.kr>
2026-08-31 15:48:08 +0900

    Add files via upload

---
+++ DAY1/05_control_statement3_switch_expression.cs
@@ -0,0 +1,26 @@
+// 핵심 : switch expression
+
+int dayofweek = 1;
+
+// 0 => "sun", 1 => "mon" 으로 변경하는 코드
+
+string s1 = "";
+
+switch(dayofweek)
+{
+	case 0: s1 = "sun"; break;
+	case 1: s1 = "mon"; break;
+	case 2: s1 = "tue"; break;
+	default : s1 = "unknown"; break;
+}
+// 위 코드는 "s1" 변수를 먼저 선언 하고, 
+// case 에서 s1 에 값을 넣는  코드
+
+// 아래 처럼도 가능합니다. - 위와 동일
+string s2 = dayofweek switch
+			{
+				0=>"sun",
+				1=>"mon",
+				2=>"tue",
+				_=>"unknown"
+			};
