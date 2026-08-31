56b61c12c5bafe7eadbac8e8ecdfd9dc132b364b
ecourse <smkang@codenuri.co.kr>
2026-08-31 15:43:40 +0900

    Add files via upload

--- DAY1/05_control_statement2_switch.cs
+++ DAY1/05_control_statement2_switch.cs
@@ -9,7 +9,9 @@ switch( num  )
 {
     case 1: 
         Console.WriteLine("one");
-        break;
+        break;          // C 언어는 break 생략가능했지만
+                        // C# 은 안됨
+
     case 2:
         Console.WriteLine("two");
         break;
@@ -20,7 +22,19 @@ switch( num  )
 }
 
 
-
 object obj = 3.4;
 
+// C#  switch 는 아주 막강합니다.
+// => 패턴 매칭 기능있습니다.
+
+switch(obj)
+{
+    case 1: break;  // 값조사
+            
+    case double:    // 타입 조사
+        Console.WriteLine("double"); 
+        break; 
+
+    // default 는 생략가능
+}
 
