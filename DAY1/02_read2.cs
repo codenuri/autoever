4e17b9b5cc3d0c6f83fc3b67df2a4495b91154db
ecourse <smkang@codenuri.co.kr>
2026-08-31 12:17:40 +0900

    Add files via upload

--- DAY1/02_read2.cs
+++ DAY1/02_read2.cs
@@ -1,10 +1,28 @@
-using System;
+// using System;
 
-// 핵심 : 정수/실수 입력
+// C# 내부
+/*
+namespace System
+{
+    class Console
+    {
+        // 출력과 관련된 다양한 정적 메소드
+        // 입력과 관련된 다양한 정적 메소드
+    }
+}
+*/
 
-Console.Write("input yout age >> ");
+// Console 창에 대한 모든 작업은 
+// => Console 클래스에서 메소드 찾으세요
+Console.Clear(); // 화면 지우기 
 
-string s = Console.ReadLine(); // "10"
+// 화면 출력
+Console.Write("input yout name >> ");
+
+// 사용자 입력 : ReadLine 메소드
+string s = Console.ReadLine();
+
+Console.WriteLine("입력된 이름 {0}", s);
 
 
 
