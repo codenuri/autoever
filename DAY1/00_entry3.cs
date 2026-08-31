09a568a259e25d934777bb4ce07bca5a3a3bd58b
ecourse <smkang@codenuri.co.kr>
2026-08-31 11:25:29 +0900

    Add files via upload

---
+++ DAY1/00_entry3.cs
@@ -0,0 +1,15 @@
+﻿// Top Level 방식의 원리 ( 교재 6 page )
+
+// System.Console.WriteLine("hello, C#");
+
+
+// 위 한줄을 컴파일러가 아래 처럼 수정한것
+// => 컴파일러가 Main 을 만드는 것
+
+class CompilerGeneratedName   // <- 컴파일러가 임의의 이름을 사용
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("hello, C#");
+    }
+}
