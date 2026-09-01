1491891a3426404ef6b0d3907b94b07267b83420
ecourse <smkang@codenuri.co.kr>
2026-09-01 15:03:29 +0900

    Add files via upload

---
+++ DAY2/07_static1.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+// static 멤버 개념
+// => 대부분의 객체지향 프로그래밍 언어의 공통 개념
+
+
+// Car 객체(자동차)를 몇대나 만들었는지 알고 싶다
+
+class Car 
+{
+    private int speed = 0;
+    private int color = 0;
+
+    public Car(int s)
+    {
+        speed = s;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Car c1 = new Car(50);
+        Car c2 = new Car(80);
+
+        // 현재까지 실행하면서 자동차 객체를 몇개 생성했는지 알고싶다 !!
+        // => 다음 소스부터 해결
+    }
+}
