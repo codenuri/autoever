bd0074c1d15eee01d42678e6344917a2ebcc155a
ecourse <smkang@codenuri.co.kr>
2026-09-01 09:55:48 +0900

    Add files via upload

--- DAY2/03_oop3.cs
+++ DAY2/03_oop3.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System.Drawing;
+using static System.Console;
 
 // 프로그램에서 Rect 타입을 많이 사용한다.
 
@@ -22,3 +23,29 @@ class Program
 //        Console.WriteLine("면적 : {0}", area); // 0
     }
 }
+
+/*
+namespace System
+{
+    namespace Drawing
+    {
+        class Rectangle { }
+    }
+}
+*/
+
+//-----------------------------------------
+// C# 언어를 통해서 배워야 하는 것
+
+// 1. 프로그래밍 관용구를 C# 언어는 어떻게 표현하는 가 ?
+// => 변수, 배열, 함수, 제어문 등의 C# 표현
+
+
+// 2. 객체지향 프로그래밍 문법
+// => class 를 사용해서 "타입을 만들때 사용하는 문법들"
+
+
+// 3. 이미 만들어진 타입을 사용하는 방법
+// => 표준 라이브러리 활용
+// => 위의 Rectangle 타입에는 어떤 메소드가 있는가 ?
+// => Rectagle 외에 어떤 타입이 있고 어떤 메소드가 있는가 ?
