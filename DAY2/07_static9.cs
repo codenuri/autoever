dff30406d23ddbfd9289445c095056d5bba5d591
ecourse <smkang@codenuri.co.kr>
2026-09-01 16:31:24 +0900

    Add files via upload

--- DAY2/07_static9.cs
+++ DAY2/07_static9.cs
@@ -43,9 +43,20 @@ class Program
 //      Date d = new Date(2026, 5, 1);
 //      int ds1 = d.HowManyDays();
 
-
         // #2. static 메소드로 제공
         int ds2 = Date.HowManyDays(5);
+
+        //---------------------------------
+        // 내일 날짜를 구하고 싶습니다.
+        // => 특정 날짜와 연관된 연산입니다
+        // => instance method 가 좋습니다.
+        // => 복습할때 구현해 보세요 
+        Date d3 = new Date(2026, 9, 1);
+        Date d4 = d3.Tomorrow();
+
+        // static method 로 하면 아래 처럼해야 합니다.
+        // => 좋지 않은 선택
+        Date d5 = Date.Tomorrow(2026, 9, 1);
 
     }
 }
