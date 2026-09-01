84d68bca631aeeddf03a102b2c11308aed15cd5e
ecourse <smkang@codenuri.co.kr>
2026-09-01 10:50:39 +0900

    Add files via upload

--- DAY2/05_private2.cs
+++ DAY2/05_private2.cs
@@ -8,10 +8,15 @@ class Bike
 
     public void SetGear(int g)
     {
-        gear = g;
+        // 자신의 상태를 변경하기 전에 인자값의 유효성을 먼저 확인합니다.
+        // => 아래 코드는 인자값이 유효한 경우만 자신의 상태 변경
+        // => 그외에는 무시하는 코드 ( 보통은 예외 발생이 더 좋은 방법)
+        if (g > 0 && gear < 20)
+        {
+            gear = g;
+        }
     }
 }
-
 class Program
 {
     public static void Main()
