d88e8c537a1724afe5e7e127a5462eae0193bce4
ecourse <smkang@codenuri.co.kr>
2026-09-01 12:21:55 +0900

    Add files via upload

---
+++ DAY2/wpf3.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+// 핵심 : Window 클래스로 GUI 만들기
+
+class Program
+{
+    public static void Main()
+    {
+        Window w = new Window();
+
+        w.Show();
+
+        MessageBox.Show("Hello, WPF");
+    }
+}
