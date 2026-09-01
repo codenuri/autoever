da5296d86869a1557898a2792951d7e26c5c4a3b
ecourse <smkang@codenuri.co.kr>
2026-09-01 11:39:55 +0900

    Add files via upload

---
+++ DAY2/06_property5.cs
@@ -0,0 +1,30 @@
+﻿class Person
+{
+    private int age;
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public int Person() => age = 0;      // 인자 없는 생성자
+    public int Person(int a) => age = a; // 인자 1개 생성자
+}
+class Program
+{
+    public static void Main()
+    {
+        Person p1 = new Person();   // 인자 없는 생성자 호출
+        Person p2 = new Person(20); // 인자 1개 생성자 호출
+        Person p3 = new Person { Age = 20 };
+                        // 1. Person p3 = new Person()
+                        // 2. p3.Age = 20
+
+        // 여러개 property 가 있으면 아래 처럼도 가능
+//      Person p4 = new Person { Age = 20, Name="kim" };
+
+    }
+}
+
+
