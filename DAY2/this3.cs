c64afa8743f2ec00da03681eb8808f1b68851f96
ecourse <smkang@codenuri.co.kr>
2026-09-01 14:37:02 +0900

    Add files via upload

---
+++ DAY2/this3.cs
@@ -0,0 +1,26 @@
+﻿class Point
+{
+    private int x = 0;
+    private int y = 0;
+
+    // this 활용 #2. this 를 반환 하는 함수
+    // => 자바에서 널리 사용하는 "빌더 기술"
+    public Point SetX(int a)
+    {
+        x = a;
+        return this;
+    }
+    public Point SetY(int a)
+    {
+        y = a;
+        return this;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Point p = new Point();
+        p.SetX(3);
+    }
+}
