65cbf642b2b44f8ae5b11b1c895054f84faae28c
ecourse <smkang@codenuri.co.kr>
2026-09-01 14:08:55 +0900

    Add files via upload

--- DAY2/06_property7.cs
+++ DAY2/06_property7.cs
@@ -1,7 +1,7 @@
 ﻿class Person
 {
     /*
-    private int age;
+    private int age = 0;
     public int Age
     {
         get { return age; }
@@ -14,6 +14,14 @@
     // => 아래 한줄이 위 주석 코드와 완전히 동일
     // => auto-implemented property 라고 합니다.
     public int Age { get; set; } = 0;
+
+    // 추가적인 로직이 없다면 "public 필드"로 해도 되지 않나요?
+    // => public 필드 보다 위 코드가 훨씬 좋습니다.
+
+    // 현재는 추가로직이 없지만
+    // => 미래에 추가로직이 필요하면
+    // => 이 클래스만 수정하면 되고
+    // => Main 코드는 수정될필요 없습니다.
 }
 
 class Program
