9a1f9b3f2481491cc3f6a2898ea4040e57135362
ecourse <smkang@codenuri.co.kr>
2026-09-01 10:48:10 +0900

    Add files via upload

---
+++ DAY2/05_private2.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+class Bike
+{
+    // private 필드 : 자신의 메소드 안에서만 접근 가능하고
+    //                외부 함수에서는 접근 할수 없다
+    private int gear = 0;
+
+    public void SetGear(int g)
+    {
+        gear = g;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Bike b = new Bike();
+
+//      b.gear = -10;  // error
+
+        b.SetGear(-10);
+    }
+}
+
+
