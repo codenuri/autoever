f8540f1a61c35afd20156e7ca25382e3eef97905
ecourse <smkang@codenuri.co.kr>
2026-09-01 14:30:08 +0900

    Add files via upload

---
+++ DAY2/this2.cs
@@ -0,0 +1,24 @@
+﻿class Point
+{
+    private int x = 0;
+    private int y = 0;
+    
+    // this 활용 #1. 이름 충돌이 있을때 필드 임을 명확히 하기 위해
+    public void Set(int x, int y)     
+    {                                 
+        x = x;                        
+        y = y;                        
+    }                                 
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Point p1 = new Point();
+        Point p2 = new Point();
+
+        p1.Set(1, 2); 
+        p2.Set(1, 2);
+    }
+}
