df93fbfb1a0372660670e7f1929e79bb39e2787b
ecourse <smkang@codenuri.co.kr>
2026-09-01 14:12:34 +0900

    Add files via upload

---
+++ DAY2/this1.cs
@@ -0,0 +1,24 @@
+﻿//this1.cs 
+
+class Point
+{
+    private int x = 0;
+    private int y = 0;
+
+    public void Set(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Point p1 = new Point();
+        Point p2 = new Point();
+
+        p1.Set(1, 2);
+        p2.Set(1, 2);
+    }
+}
