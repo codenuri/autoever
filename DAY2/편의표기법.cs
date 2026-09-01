e480e741b2a5f577dd2bc9427475a932b63b43fe
ecourse <smkang@codenuri.co.kr>
2026-09-01 10:16:43 +0900

    Add files via upload

--- DAY2/편의표기법.cs
+++ DAY2/편의표기법.cs
@@ -10,3 +10,13 @@ string s = new string("ABC");
 
 Rectangle r = new Rectangle(0, 0, 5, 5);
 
+// 규칙 #2. 일부 표준 타입에 대해서, 다른 언어와 유사하게 간략히 사용할수 있도록
+//          "편의 표기법" 을 제공합니다.
+int    n1 = 0;  // int n1 = new int() 와 동일
+double d1 = 0.0;
+string s2 = "ABC";
+
+// Rectange 은 컴파일러가 인식하는 타입(표준 타입)이 아닌 
+// class 문법으로 만든 타입 
+// => 편의 표기법 안됨. 반드시 new 필요
+
