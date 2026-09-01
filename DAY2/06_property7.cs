15054103f03042fb803063a662ed7f7d36d4b443
ecourse <smkang@codenuri.co.kr>
2026-09-01 14:03:05 +0900

    Add files via upload

---
+++ DAY2/06_property7.cs
@@ -0,0 +1,24 @@
+﻿class Person
+{
+    private int age;
+
+    // set, get 중 한개만 만들어도 됩니다.
+    // 아래 코드는 쓰기 전용 프라 퍼티
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Person p = new Person();
+
+        p.Age = 25;    
+        int n = p.Age; 
+   }
+}
+
+
