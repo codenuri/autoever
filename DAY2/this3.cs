054fe39ae88a8b9c3fa3dcf938c48fdcab2b48eb
ecourse <smkang@codenuri.co.kr>
2026-09-01 14:43:14 +0900

    Add files via upload

--- DAY2/this3.cs
+++ DAY2/this3.cs
@@ -21,6 +21,14 @@ class Program
     public static void Main()
     {
         Point p = new Point();
-        p.SetX(3);
+
+        // 메소드가 this 를 반환하면 메소드 호출을 연속적으로 할수 있습니다.
+        // => Method chainging 이라는 기술입니다.
+        // => 요즘 다양한 언어에서 아주 널리 사용되는 기술입니다.
+        p.SetX(3).SetY(3).SetX(3).SetY(5);
+
+        // java 에서 아래 처럼 많이 사용합니다
+        // Person p2 = new Person();
+        // p2.SetName("kim").SetAge(30).SetHegith(180);
     }
 }
