68e61aaf7f4558fbbce34621421e4e4005d68938
ecourse <smkang@codenuri.co.kr>
2026-09-01 16:25:01 +0900

    Add files via upload

---
+++ DAY2/07_static9.cs
@@ -0,0 +1,47 @@
+﻿class Date
+{
+    private int year = 0;
+    private int month = 0;
+    private int day = 0;
+
+    private static int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+
+    public int Month
+    {
+        get { return month; }
+        set
+        {
+            if (value < 1 || value > 12)
+                throw new Exception();
+            month = value;
+        }
+    }
+
+    public Date(int y, int m, int d) => (year, month, day) = (y, m, d);
+
+    public Date AddDays(int ds)
+    {
+        Date temp = new Date(year, month, day + ds); // 잘못된 구현
+
+        return temp;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        // 5월달이 몇일까지 있는지 알고 싶다
+
+        // 다음중 사용하기 편리해 보이는 것은 ?
+
+        // #1. instance 메소드로 제공
+        Date d = new Date(2026, 5, 1);
+        int ds1 = d.HowManyDays();
+
+
+        // #2. static 메소드로 제공
+        int ds2 = Date.HowManyDays();
+
+    }
+}
