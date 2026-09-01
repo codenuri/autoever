1d240129a0593a7f4b7a02a7689b192eadfe67dd
ecourse <smkang@codenuri.co.kr>
2026-09-01 10:13:11 +0900

    Add files via upload

---
+++ DAY2/편의표기법.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+// 편의표기법.cs
+
+// 규칙 #1. C# 에서 모든 종류의 객체(변수) 을 만들때는 new 를 사용합니다.
+
+int    n = new int();
+double d = new double();
+string s = new string("ABC");
+
+Rectangle r = new Rectangle(0, 0, 5, 5);
+
