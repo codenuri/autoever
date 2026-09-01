8cf912a3269df323463a263ac877005efe5a5cdd
ecourse <smkang@codenuri.co.kr>
2026-09-01 12:25:22 +0900

    Add files via upload

--- DAY2/wpf3.cs
+++ DAY2/wpf3.cs
@@ -3,13 +3,16 @@
 // 핵심 : Window 클래스로 GUI 만들기
 
 class Program
-{
+{    
+    [STAThread]     // Main 함수에서 GUI 만들때 필요. 관례적 코드. 항상동일
     public static void Main()
     {
         Window w = new Window();
 
         w.Show();
 
-        MessageBox.Show("Hello, WPF");
+        // 아래 코드가 없으면 프로그램은 즉시 종료되므로
+        // 윈도우가 바로 파괴 되고 종료 됩니다.
+//      MessageBox.Show("Hello, WPF");
     }
 }
