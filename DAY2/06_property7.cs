9376f56b80a1b36fadc27a9f5b7b219de483bac7
ecourse <smkang@codenuri.co.kr>
2026-09-01 14:05:57 +0900

    Add files via upload

--- DAY2/06_property7.cs
+++ DAY2/06_property7.cs
@@ -1,15 +1,21 @@
 ﻿class Person
 {
+    /*
     private int age;
-
-    // set, get 중 한개만 만들어도 됩니다.
-    // 아래 코드는 쓰기 전용 프라 퍼티
     public int Age
     {
         get { return age; }
         set { age = value; }
     }
+    */
+
+    // 위 처럼 별도의 logic 을 추가로 가지지 않은 프라퍼티가 필요하면
+    // 아래 처럼 한줄만 만들면 됩니다.
+    // => 아래 한줄이 위 주석 코드와 완전히 동일
+    // => auto-implemented property 라고 합니다.
+    public int Age { get; set; } = 0;
 }
+
 class Program
 {
     public static void Main()
