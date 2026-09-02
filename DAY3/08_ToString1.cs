2f8b405fec2ad7e7cc1be5bd265d8f280541b60d
ecourse <smkang@codenuri.co.kr>
2026-09-02 15:29:45 +0900

    Add files via upload

--- DAY3/08_ToString1.cs
+++ DAY3/08_ToString1.cs
@@ -8,3 +8,13 @@ Console.WriteLine(n);   // 인자로 int 객체 전달
 Console.WriteLine(d);   // 인자로 double 객체 전달
 
 // #2. 원리가 뭘까요 ?
+void MyWriteLine(object obj)
+{
+    // obj 의 상태를 문자열로 변경
+    string s = obj.ToString();
+
+    // 이제 문자열 s 를 화면에 출력
+    Console.WriteLine(s); // 실제는 내부 함수 사용해서 화면에 출력
+}
+MyWriteLine(n);
+MyWriteLine(d);
