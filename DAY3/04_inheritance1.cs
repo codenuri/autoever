58a3aa553b65dce48611acdf20ffae44b0c645e5
ecourse <smkang@codenuri.co.kr>
2026-09-02 11:11:06 +0900

    Add files via upload

---
+++ DAY3/04_inheritance1.cs
@@ -0,0 +1,34 @@
+﻿// inheritance (상속 )
+// => 기존의 타입을 확장해서 새로운 타입을 만드는 문법
+
+// 용어
+// Person : Base class,    Parent class, Super class
+// Student: Derived class, Child class,  Sub class
+
+
+// 1. 모든 사람의 공통의 특징을 먼저 타입으로 설계
+class Person
+{
+    private string name;
+    private int age;
+}
+
+// 2. Person 타입을 확장해서 새로운 타입을 설계
+class Professor : Person 
+{
+    private string major;
+}
+
+class Student : Person
+{
+    private string id;
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Student s = new Student();
+
+    }
+}
