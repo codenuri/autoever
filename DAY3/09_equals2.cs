77097ca107c9aefe9ea3e201c94768f2193908ff
ecourse <smkang@codenuri.co.kr>
2026-09-02 17:03:30 +0900

    Add files via upload

--- DAY3/09_equals2.cs
+++ DAY3/09_equals2.cs
@@ -18,10 +18,12 @@ class Program
         Point p4 = new Point(1, 2);
 
         // 동일성을 조사하는 방법 #1. == 연산자
+        // => 객체의 동일성을 조사하는 코드
+        Console.WriteLine("{0}", p1 == p2); // True
+        Console.WriteLine("{0}", p3 == p4); // False
 
-        // 결과 확인해서 == 의 기능은 "객체의 동일" 조사인지 "상태의 동일" 조사인지
-        // 확인해 보세요
-        Console.WriteLine("{0}", p1 == p2);
-        Console.WriteLine("{0}", p3 == p4);
+        // 방법 #2. object 에서 상속받은 Equals() 가상메소드
+        Console.WriteLine("{0}", p1.Equals(p2));
+        Console.WriteLine("{0}", p3.Equals(p4));
     }
 }
