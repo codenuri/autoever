ada3429971d42b21e7a61f4f7ade5a59aa00686a
ecourse <smkang@codenuri.co.kr>
2026-09-02 17:20:37 +0900

    Add files via upload

--- DAY3/09_equals3.cs
+++ DAY3/09_equals3.cs
@@ -24,9 +24,22 @@ class Program
         Point p3 = new Point(1, 2);
         Point p4 = new Point(1, 2);
 
-        // p3 와 p4 의 상태가 동일한지 판단하는 최선의 코드를 만드세요
 
-        bool ret; // <= 결과를 여기에 담으세요
+        // p3 와 p4 의 상태가 동일한지 판단하는 최선의 코드를 만드세요
+        
+        bool ret = p3 == p4; // 같은 객체인가 ? 같은 객체는 상태도 동일
+
+        // 동일 객체가 아닌 경우만 상태 조사
+        if (ret == false)
+        {
+            ret = p3.Equals(p4);
+        }
+        
+        //-----------------------------------
+        // 아래 한줄이 위코드와 동일
+        // object 클래스의 "Equals" static method
+        bool ret2 = object.Equals(p3, p4); // 
+
 
 
 
