4df181470d6a3637f0b23ded5faa7b346210b7ee
ecourse <smkang@codenuri.co.kr>
2026-09-02 14:29:52 +0900

    Add files via upload

--- DAY3/07_example4.cs
+++ DAY3/07_example4.cs
@@ -1,6 +1,20 @@
 ﻿using static System.Console;
 
-// 핵심 #4
+// 핵심 #4. Shape 타입인 e 를 Rect, Circle 로 캐스팅해서 사용하는 코드
+// => Main 함수 에서 "if ( cmd == 9 )" 코드 참고
+// => 이 코드의 문제점은 무엇일까요 ?
+
+// 핵심 #5. OCP 규칙 - 객체지향 프로그래밍 설계의 핵심 원리 5개 중 한개
+// => 기능 확장에는 열려있고(Open, 모듈/클래스 등이 추가되어도)
+// => 코드 수정에는 닫혀야 (Close, 기존 코드는 수정되지 않도록)
+// => 만들라는 원칙(Principle)
+
+// Open Close Principle( 개방폐쇄의 법칙)
+
+// 아래 코드는 Triangle 이 추가되면 Main 의 if 문이 변경됩니다.
+// => OCP 위반
+// => 나쁜 디자인
+
 
 class Shape
 {
