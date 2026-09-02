eb4c97f87323b9a2d61b77dcdb023a482d0b55a0
ecourse <smkang@codenuri.co.kr>
2026-09-02 15:18:00 +0900

    Add files via upload

--- DAY3/07_example7.cs
+++ DAY3/07_example7.cs
@@ -5,10 +5,16 @@
 class Shape
 {
     private int color = 0;
-
     public virtual void Draw() { WriteLine("draw Shape"); }
     public void SetColor(int c) { color = c; }
     public virtual int GetArea() { return -1; }
+
+    public virtual Shape Clone()
+    {
+        Shape s = new Shape();
+        s.color = color; // 모든 필드를 자신과 동일하게 설정
+        return s;
+    }
 }
 
 
@@ -18,10 +24,13 @@ class Shape
 class Rect : Shape
 {
     public override void Draw() { WriteLine("draw Rect"); }
+    public override Shape Clone() { return new Rect(); }
 }
+
 class Circle : Shape
 {
     public override void Draw() { WriteLine("draw Circle"); }
+    public override Shape Clone() { return new Circle(); }
 }
 class Program
 {
@@ -53,6 +62,25 @@ class Program
                 // 어떻게 구현해야 할까요 ?
                 // k 번째 도형은 뭘까요 ?
 
+                // 방법 #1. is 연산자로 조사하자
+                // => 가능한 방법이지만 Triangle 추가시 코드가 변경(추가)된다
+                // => OCP 를 만족할수 없는 나쁜 디자인
+                /*
+                if ( s[k] is Rect )
+                {
+                    // s[k] 번째 복사본 만들에서 s 에 추가
+                }
+                else if ( s[k] is Circle )
+                {
+                }
+                */
+
+                // 방법 #2. Don't Ask Do It
+                // => 물어보지 말고, 시켜라
+
+                Shape t = s[k].Clone();
+
+                s.Add(t);
 
             }
 
