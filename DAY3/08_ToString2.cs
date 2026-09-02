a952f600c096d622259b82f675ca5b0b54f3a9f7
ecourse <smkang@codenuri.co.kr>
2026-09-02 15:36:15 +0900

    Add files via upload

---
+++ DAY3/08_ToString2.cs
@@ -0,0 +1,21 @@
+using static System.Console;
+
+class Point 
+{
+	private int x = 0;
+	private int y = 0;
+
+	public Point(int a, int b) => (x, y) = (a, b);
+}
+
+class Program 
+{
+	public static void Main()
+	{
+		Point p = new Point(1, 2);
+
+		WriteLine(p);	// 내부적으로 
+						// 1. string s = p.ToString() 으로 문자열을 얻고
+						// 2. s 를 화면에 출력
+    }
+}
