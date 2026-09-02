5ebd4c0e755600d2cf68d5065a030fe7ab15342d
ecourse <smkang@codenuri.co.kr>
2026-09-02 15:27:22 +0900

    Add files via upload

---
+++ DAY3/08_ToString1.cs
@@ -0,0 +1,10 @@
+﻿// 08_ToString1.cs
+
+int n = 0;
+double d = 3.4;
+
+// #1. Console.WriteLine() 은 인자로 거의 모든 타입의 객체를 받을수 있습니다.
+Console.WriteLine(n);   // 인자로 int 객체 전달
+Console.WriteLine(d);   // 인자로 double 객체 전달
+
+// #2. 원리가 뭘까요 ?
