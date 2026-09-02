c2b7794f742eea297b93575114820bd516b75ea3
ecourse <smkang@codenuri.co.kr>
2026-09-02 17:34:34 +0900

    Add files via upload

---
+++ DAY3/09_equals5.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+class Point
+{
+    private int x = 0;
+    private int y = 0;
+    public Point(int a, int b) => (x, y) = (a, b);
+
+    public override bool Equals(object obj)
+    {
+        Point other = (Point)obj;
+        return x == other.x && y == other.y;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Point p1 = new Point(1, 2);
+        Point p2 = p1;
+
+        Point p3 = new Point(1, 2);
+        Point p4 = new Point(1, 2);
+
+    }
+
+
+}
+
