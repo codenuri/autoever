e2e76771ea3df636e6c59d0afc7d23a8e625a1b2
ecourse <smkang@codenuri.co.kr>
2026-09-02 11:21:57 +0900

    Add files via upload

---
+++ DAY3/05_upcasting1.cs
@@ -0,0 +1,27 @@
+﻿class Animal
+{
+    public int age = 0;
+}
+
+class Dog : Animal
+{
+    public int color = 0;
+}
+
+class Cat : Animal
+{
+    public int speed = 0;
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Dog r1 = new Dog(); // ?
+
+        int r2 = new Dog(); // ?
+
+        Animal r3 = new Dog(); // ?
+
+    }
+}
