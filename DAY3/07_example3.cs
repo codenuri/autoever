ee2b587a08b6d30547a959b6908dfb269f931e75
ecourse <smkang@codenuri.co.kr>
2026-09-02 14:19:43 +0900

    Add files via upload

--- DAY3/07_example3.cs
+++ DAY3/07_example3.cs
@@ -1,5 +1,7 @@
 ﻿using static System.Console;
 
+// 핵심 #3. 아래 main 의 "e.Draw()" 가 에러가 발생하는 이유
+// => 해결책은 다음 소스에서
 
 class Shape
 {
@@ -28,11 +30,12 @@ class Program
             else if ( cmd == 2 ) s.Add(new Circle());
             else if ( cmd == 9 )
             {
-                foreach (var e in s)
-                    e.Draw();
+                foreach (var e in s) // 여기서 e 의 타입은 Shape
+                    e.Draw();        // Shape 안에 Draw 는 없습니다
+                                     // 그래서 error
             }
         }
     }
 }
-// 컴파일 하면 왜 어디서? 왜 에러가 발생할까요 ?
+// 컴파일 하면 에러 입니다. 왜 어디서? 왜 에러가 발생할까요 ?
 // 해결책은 뭘까요 ?
