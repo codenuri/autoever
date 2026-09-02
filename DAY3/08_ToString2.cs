bc525f0de1c1ec0c00bb6e2ff4fa52f485d9cdbf
ecourse <smkang@codenuri.co.kr>
2026-09-02 15:42:09 +0900

    Add files via upload

--- DAY3/08_ToString2.cs
+++ DAY3/08_ToString2.cs
@@ -16,6 +16,12 @@ class Point // : object
 	private int y = 0;
 
 	public Point(int a, int b) => (x, y) = (a, b);
+
+    public override string ToString()
+    {
+		// 자신의 상태를 문자열로 만들어서 반환
+		return string.Format("x = {0}, y = {1}", x, y);
+    }
 }
 
 class Program 
@@ -29,3 +35,14 @@ class Program
 						// 2. s 를 화면에 출력
     }
 }
+
+// object 클래스에는 
+// => 객체의 상태를 문자열로 변경하기 위한 ToString() 가상 메소드 제공
+
+// => 그런데, 기본 구현은 "클래스 이름" 을 문자열로 반환
+// => 사용자가 자신의 타입을 만들때 이 기능을 다시 만들고 싶으면
+// => ToString() 을 override 해서 상태(x,y) 를 문자열로 반환하세요
+
+
+
+
