568e1957ae431e59ab6b900459e247fcec124cea
ecourse <smkang@codenuri.co.kr>
2026-09-02 16:27:29 +0900

    Add files via upload

---
+++ DAY3/wpf3.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+class MainFrame : Window
+{
+    protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+    {
+        Point pt = e.GetPosition(this);    
+
+        Console.WriteLine("LButtonDown : {0}, {1}", pt.X, pt.Y);
+    }
+
+    protected override void OnMouseRightButtonDown(MouseButtonEventArgs e)
+    {
+        Console.WriteLine("RButtonDown");
+    }
+}
+
+// WPF 의 핵심 클래스 2개
+// Window : 윈도우 창(GUI) 를 만들기 위해서 제공
+//          => 보통 파생 클래스 만들어서 사용
+
+// Application : GUI 아님. 프로그램시작, 종료, 이벤트 루프등을 담당
+//          => 보통 파생 클래스를 만들어서 사용
+
+class App : Application
+{
+
+}
+
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        MainFrame w = new MainFrame();
+        w.Show();
+
+        Application app = new Application();
+        app.Run();
+    }
+}
