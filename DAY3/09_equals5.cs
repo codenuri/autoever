6c62e0d55bd7aa4f1459caa5afa03ab452c6671c
ecourse <smkang@codenuri.co.kr>
2026-09-02 17:39:22 +0900

    Add files via upload

--- DAY3/09_equals5.cs
+++ DAY3/09_equals5.cs
@@ -23,6 +23,21 @@ class Program
         Point p3 = new Point(1, 2);
         Point p4 = new Point(1, 2);
 
+        // 핵심 #1. 동일한 객체이지 조사하고 싶다
+        bool ret1 = object.ReferenceEquals(p1, p2); // 최선의 코드
+        bool ret2 = p1 == p2;   // 일반적으로 사용가능
+                                // Point 타입이 == 를 다시 만들었다면
+                                // 동일 객체라는 보장 없음
+
+        // 핵심 #2. 상태가 동일한지 조사
+        // => object 에서 물려받은 Equals 가상함수를 override 한 타입만 조사 가능
+
+        bool ret3 = object.Equals(p3, p4); // 최선의 코드
+                                            // p3 == p4 조사후 false 라면
+                                            // p3.Equals(p4)
+        bool ret4 = p3.Equals(p4); // 가능하지만 최선은 아님
+
+
     }
 
 
