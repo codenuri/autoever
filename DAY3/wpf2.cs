ed4d27b387f1d265ae7e8523d97f2ce7e49ddbd1
ecourse <smkang@codenuri.co.kr>
2026-09-02 16:23:21 +0900

    Add files via upload

--- DAY3/wpf2.cs
+++ DAY3/wpf2.cs
@@ -20,11 +20,19 @@ class MainFrame : Window
 {
     protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
     {
-        Console.WriteLine("LButtonDown");
+        // this : 현재 객체(윈도우)의 포인터
+        Point pt = e.GetPosition(this);    // 내 윈도우를 기준으로 좌표를 알려달라
+
+        Console.WriteLine("LButtonDown : {0}, {1}", pt.X, pt.Y);
+    }
+
+    // 잘되신 분은 마우스 오른쪽 버튼 누를때 로깅해 보세요
+
+    protected override void OnMouseRightButtonDown(MouseButtonEventArgs e)
+    {
+        Console.WriteLine("RButtonDown");
     }
 }
-
-
 
 class Program
 {
