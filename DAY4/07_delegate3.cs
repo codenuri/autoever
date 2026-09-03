9a900a2cc5b73f1f328bfe6a632bdeb5db9f8538
ecourse <smkang@codenuri.co.kr>
2026-09-03 14:19:32 +0900

    Add files via upload

--- DAY4/07_delegate3.cs
+++ DAY4/07_delegate3.cs
@@ -15,14 +15,13 @@ class Program
         f.Invoke(10);   // 2. invoke() 메소드 사용
 
         // #2. =, +=, -= 등으로 여러개 함수도 등록 가능
+        // => 단, signature( 메소드 반환타입, 인자 모양)이 동일한 메소드만 등록가능
         MyType f1 = Foo;
         f1 += Goo;
         f1 += Hoo;
 
         f1(0); // 등록된 3개의 메소드 호출
     }
-
-
 
     public static void Foo(int arg) { WriteLine($"Foo : {arg}"); }
     public static void Goo(int arg) { WriteLine($"Goo : {arg}"); }
