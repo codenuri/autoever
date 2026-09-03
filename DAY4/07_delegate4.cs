58caa1eb5dc167252add9fc4f6a76498106c5b1b
ecourse <smkang@codenuri.co.kr>
2026-09-03 14:25:23 +0900

    Add files via upload

--- DAY4/07_delegate4.cs
+++ DAY4/07_delegate4.cs
@@ -6,8 +6,9 @@
 
         // 배열에서 2가 몇번째 있는지 알고 싶다
         // => 배열에 대한 모든 연산은 "Array" 클래스의 정적 메소드에서 찾으세요
+        int ret1 = Array.IndexOf(x, 2);
 
-
+        Console.WriteLine("{0}", ret1); // 1
     }
 
 }
