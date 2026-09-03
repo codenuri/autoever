e39cb57a83baecb48e0cdb19b290140f05b985db
ecourse <smkang@codenuri.co.kr>
2026-09-03 10:34:40 +0900

    Add files via upload

---
+++ DAY4/정리.cs
@@ -0,0 +1,30 @@
+﻿// 정리.cs
+
+// C# 에서 알아야 하는 것들
+
+// 1. 프로그래밍 관용구에 대한 C# 표현
+// => 변수, 배열, 제어문(if, switch), 함수 만드는 방법등
+
+// 2. class 문법
+// => private, public
+// => 생성자
+// => Property
+// => Value Type vs Reference Type
+// => static field, static method
+
+// 3. 상속(inheritance) 문법
+// => 기본 개념
+// => upcasting  Animal a = new Dog(), List<Animal> 은 모든 동물 보관
+// => 가상함수 특징 ( static binding, dynamic binding)
+// => abstract class, abstract method
+// => interface
+// => 용어 : OCP, 강한 결합, 약한 결합
+// => SOLID : 객체지향 프로그래밍에서 지켜야 하는 5개 원칙
+//            SRP, OCP, LSP, ISP, DIP 의 1글자 만 합해서 부르는 말
+
+// ----- 여기 까지가 기본 적인 객체지향 문법 ---------------------
+
+// 4. C# 에서 추가로 제공하는 몇가지 문법들
+// =>
+
+// 5. 라이브러리 활용 맛보기 - WPF  <= 내일
