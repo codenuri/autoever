85eab1c3a71261c45098ec4e0477a21d14bbc105
ecourse <smkang@codenuri.co.kr>
2026-09-03 12:40:57 +0900

    Add files via upload

--- DAY4/06_generic_constraint2.cs
+++ DAY4/06_generic_constraint2.cs
@@ -13,24 +13,27 @@ class Point
 class Program
 {
     // "where T : 조건"     
-    // => generic constrant 문법
+    // => generic constraint 문법
     // => 조건만 만족하는 타입만 사용 가능하다
     // => 메소드 안에서 "조건" 이 제공하는 기능 사용가능
     public static T Max<T>(T a, T b) where T : IComparable 
     {
+        // 이안에서는 이제 IComparable 인터페이스가 제공하는 
+        // 메소드 호출 가능
         return a.CompareTo(b);
     }
 
-
     public static void Main()
     {
-        WriteLine($"{Max(10, 20)}");
-        WriteLine($"{Max("AAA", "CC")}");
+        WriteLine($"{Max(10, 20)}");        // ok int 는 IComparable 구현
+        WriteLine($"{Max("AAA", "CC")}");   // ok string 는 IComparable 구현
 
-        Foo(10, 20);
-    }
-    public static void Foo<T>(T a, T b)
-    {
+
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(2, 2);
+
+        WriteLine($"{Max(p1, p2)}"); // error. Point 은 IComparable 구현안함
+                                    // 구현 했다면 전달 가능
 
     }
 }
