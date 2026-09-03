1e1ffaea821ede3e6ed71ae96501fbb5aacab456
ecourse <smkang@codenuri.co.kr>
2026-09-03 14:16:20 +0900

    Add files via upload

---
+++ DAY4/07_delegate3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+
+delegate void MyType(int arg);
+
+class Program
+{
+    public static void Main()
+    {
+        MyType f = Foo; 
+
+        // delegate 에 보관된 메소드 호출하는 법
+        f(10);          // 1. () 연산자 사용
+        f.Invoke(10);   // 2. invoke() 메소드 사용
+    }
+
+
+
+    public static void Foo(int arg)
+    {
+        WriteLine($"Foo : {arg}");
+    }
+}
