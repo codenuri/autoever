42efef389328199de78bdf1a10a6c3c57be0de19
ecourse <smkang@codenuri.co.kr>
2026-09-03 15:19:09 +0900

    Add files via upload

--- DAY4/08_delegate_method2.cs
+++ DAY4/08_delegate_method2.cs
@@ -7,7 +7,8 @@ class Program
     public static void SMethod(int arg) => WriteLine("SMethod");
     public        void IMethod(int arg) => WriteLine("IMethod");
 
-    public static void Main()
+    // OS 가 Program.Main() 으로 호출 하므로 객체 전달 안됨
+    public static void Main()  // void Main() <== this 가 없다
     {
         // 이전예제 : 다른 클래스의 메소드 호출
         // 이번예제 : 자신의 메소드 호출에 대해서
@@ -30,11 +31,37 @@ class Program
         // Delegate 등록
         MyType f3 = pg.IMethod; // ok
 
-        pg.InstanceMethod(1);        
+        pg.InstanceMethod(1);    //   InstanceMethod(pg, 1)  
     }
 
-	public void InstanceMethod()
-	{
+	public void InstanceMethod() // void InstanceMethod(Program this)
+    {
         // #3. 자신의 인스턴스 메소드 안에서 다른 인스턴스 메소드 호출
+        IMethod(1); // ok.. ?? 객체가 필요 하지 않나요 ?
+                    // 컴파일러가 "this.IMethod(1)" 로 변경한것
+
+        this.IMethod(1); // ok
 	}
+}
+
+class Test
+{
+    private int x = 0;
+
+    public void foo()   // void goo(Test this)
+    {
+        // 이 함수 안에는 this 가 있으므로 
+        // 자신의 모든 멤버 접근 가능
+        // 컴파일러가 "this.멤버" 로 변경
+        x = 10; // this.x = 10 으로 변경된것
+                // 반드시 "누구의 x" 가 되어야 합니다.
+    }
+    public static void goo()    // void goo() <== this 없음
+    {
+        // 이 함수 안에서는 this가 없음.
+        // 그래서 모든 멤버 접근 못함
+        x = 10; // error
+                // this.x = 10 안됩니다.
+                // goo() 는 this 인자 없음
+    }
 }
