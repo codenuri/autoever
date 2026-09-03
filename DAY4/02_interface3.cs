a80c5c49cf31f08eb3c9444bf49d8948682d7095
ecourse <smkang@codenuri.co.kr>
2026-09-03 10:09:02 +0900

    Add files via upload

---
+++ DAY4/02_interface3.cs
@@ -0,0 +1,62 @@
+using static System.Console;
+
+// C# 에는 서로 다른 타입에서 같은 이름의 메소드를 제공하는 경우가 많습니다.
+// 이때 이름을 약속하기 위해 모두 인터페이스 기반으로 설계되었습니다.
+
+// C# 내부에 아래 코드 제공
+interface IComparable
+{
+	int CompareTo(object obj);
+}
+class string : IComparable
+{
+	public int CompareTo(object obj)
+	{
+		// 자신의 데이타에 맞게 구현
+	}
+}
+class Int32 : IComparable
+{
+
+    public int CompareTo(object obj)
+	{
+		// 자신의 데이타에 맞게 구현
+	}
+}
+
+
+class Program 
+{
+	public static void Main()
+	{
+		int n1 = 10;
+		int n2 = 20;
+
+		string s1 = "AAA";
+		string s2 = "BBB";
+
+		// 두 변수의 크기를 비교하는 방법. 
+
+		// #1. 비교 연산자(<, >,...) 사용
+		// => 수치 타입만 가능.
+		bool b1 = n1 < n2;	// ok
+		bool b2 = s1 < s2;  // error
+
+		// #2. CompareTo() 메소드 사용
+		// => "크기 비교" 라는 개념이 가능한 대부분의 타입은 제공
+		int ret1 = n1.CompareTo(n2); // ok  
+		int ret2 = s1.CompareTo(s2); // ok
+
+		// n1.CompareTo(n2) 의 결과값
+		// n1 > n2  이면 양수(1)
+		// n1 == n2 이면 0
+		// n1 < n2  이면 음수(-1)
+    }
+
+
+
+	public static void M1(IComparable ic)
+	{
+	}
+
+}
