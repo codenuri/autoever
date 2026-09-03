f5841580be82c026d9a414ecb5367912974ee926
ecourse <smkang@codenuri.co.kr>
2026-09-03 11:44:57 +0900

    Add files via upload

--- DAY4/03_parameter_modifier5.cs
+++ DAY4/03_parameter_modifier5.cs
@@ -10,8 +10,7 @@ class Program
         // 방법 #1. int.Parse() 
         // => 변경할수 없는 문자열 전달시 예외 발생 - 프로그램은 비정상종료
         int n1 = int.Parse("10");    // ok. 성공
-                                     //     int n2 = int.Parse("Hello"); // 실패. 예외 발생
-
+//      int n2 = int.Parse("Hello"); // 실패. 예외 발생
 
         // 방법 #2. int.TryParse()
         // => 성공/실패는 반환 값으로 알려주고
@@ -19,9 +18,15 @@ class Program
 
         int value;
         bool b1 = int.TryParse("10", out value);
+                                    // 성공시 value 에는 10
 
         bool b2 = int.TryParse("hello", out value);
+                                    // 실패시 value 는 0 으로
+                                    // 반환 값으로 false 반환
+        if ( b2 ==  false)
+        {
 
+        }
 
     }
 }
