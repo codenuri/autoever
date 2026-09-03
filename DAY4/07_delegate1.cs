b0beb727b67e93f661f5133e20fd22c17c2e5a25
ecourse <smkang@codenuri.co.kr>
2026-09-03 14:08:45 +0900

    Add files via upload

--- DAY4/07_delegate1.cs
+++ DAY4/07_delegate1.cs
@@ -2,6 +2,15 @@ using System;
 using static System.Console;
 
 
+// delegate
+// => 함수(메소드)의 호출 정보를 보관하는 타입
+
+// 만드는 법 알아 두세요
+// 1. 함수(메소드) 선언만 복사
+// 2. 반환 타입 앞에 delegate 표기
+// 3. 함수(메소드) 이름 대신 원하는 타입 이름 표기
+// void Foo(int arg)
+
 delegate void MyType(int arg);
 
 class Program
@@ -13,10 +22,8 @@ class Program
         string s = "ABCD";
 
         MyType f = Foo;
-        f(10); // Foo(10) 과 동일
-            
+        f(10); // Foo(10) 과 동일            
     }
-
     public static void Foo(int arg)
     {
         WriteLine($"Foo : {arg}");
