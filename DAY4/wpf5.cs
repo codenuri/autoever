42aa2d1445ba2b53514fb14373b6a3423d88dec4
ecourse <smkang@codenuri.co.kr>
2026-09-03 16:33:23 +0900

    Add files via upload

--- DAY4/wpf5.cs
+++ DAY4/wpf5.cs
@@ -6,6 +6,12 @@ using System.Windows.Controls;
 
 class MainFrame : Window
 {
+    // 컨트롤을 여러 메소드 에서 접근하려면 필드로 선언
+    // => 모든 메소드에서 접근 가능
+    private Button btn1 = null;
+    private Button btn2 = null;
+    private Button btn3 = null;
+
     public MainFrame()
     {
         Title = "Hello, WPF";
@@ -25,17 +31,25 @@ class MainFrame : Window
         //      sp.Children.Add(new Button { Content = "button2" });
         //      sp.Children.Add(new Button { Content = "button3" });
 
-        Button btn1 = new Button { Content = "button1" };
-        Button btn2 = new Button { Content = "button1" };
-        Button btn3 = new Button { Content = "button1" };
+        btn1 = new Button { Content = "button1" };
+        btn2 = new Button { Content = "button1" };
+        btn3 = new Button { Content = "button1" };
 
         sp.Children.Add(btn1);
         sp.Children.Add(btn2);
         sp.Children.Add(btn3);
 
         // btn1, btn2 등의 변수가 있으므로 해당 변수에 다양한 추가 작업가능
-
         btn1.Click += Btn1_Click;
+        btn2.Click += Btn2_Click;
+    }
+
+    private void Btn2_Click(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("button2 click");
+
+        // 버튼3 의 문자열을(Content) 를 "Hello"로 변경 
+        btn3.Content = "Hello";
     }
 
     private void Btn1_Click(object sender, RoutedEventArgs e)
