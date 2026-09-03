a6c385cf4b7d0ffa1b23ca8a8ec18240b41d9391
ecourse <smkang@codenuri.co.kr>
2026-09-03 10:20:10 +0900

    Add files via upload

--- DAY4/02_interface4.cs
+++ DAY4/02_interface4.cs
@@ -5,6 +5,8 @@ using static System.Console;
 // 1. 그냥 CompareTo() 메소드만 제공하는 방법
 // 2. IComparable 인터페이스를 구현하겠다고 표기하는 방법
 
+// 아래 코드는 IComparable 인터페이스를 구현하겠다고 표기 안한 경우
+// => Main 제일 아래줄에서 에러 발생
 class Label 
 {
 	private string title;	
@@ -28,7 +30,12 @@ class Program
 		Label d2 = new Label("BAD");
 
 		// 사용자 정의 타입인 Label 도 크기 비교가 되도록 해봅시다.
-		int ret = d1.compare_to(d2);
+		int ret = d1.CompareTo(d2);
+
+		M1(d1); // error
+	}
+	public static void M1(IComparable obj)
+	{
 	}
 }
 
