65c6ec1b9715a7b555f3e5f6dec55e9ce537e0fb
ecourse <smkang@codenuri.co.kr>
2026-09-03 10:24:17 +0900

    Add files via upload

---
+++ DAY4/02_interface5.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection.Emit;
+using static System.Console;
+
+class Label : IComparable
+{
+    private string title;
+    public Label(string s) => title = s;
+
+
+    // IComparable 안에 있는 CompareTo 는 인자가
+    // "object" 타입 입니다.
+    // 자신에 타입에 맞게 캐스팅해서 사용
+    public int CompareTo(object? obj)
+    {
+        Label other = (Label)obj;
+
+        return title.CompareTo(other.title);
+    }
+}
+
+
+
+
+class Program
+{
+    public static void Main()
+    {
+        Label d1 = new Label("GOOD");
+        Label d2 = new Label("BAD");
+
+        int ret = d1.CompareTo(d2);
+
+        M1(d1); // error
+    }
+    public static void M1(IComparable obj)
+    {
+    }
+}
+
