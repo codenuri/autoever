e554461a2ff92e4a25f696c1333994805dc5cb87
ecourse <smkang@codenuri.co.kr>
2026-09-03 15:59:23 +0900

    Add files via upload

---
+++ DAY4/wpf1.cs
@@ -0,0 +1,19 @@
+﻿// wpf1.cs
+
+using System.Windows;
+
+class MainFrame : Window
+{
+}
+class App : Application
+{
+    [STAThread]
+    public static void Main()
+    {
+        MainFrame w = new MainFrame();
+        w.Title = "Hello, WPF";
+
+        App app = new App();
+        app.Run();
+    }
+}
