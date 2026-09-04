500064cc95f0b096d4758daf539d72eb4d6850a0
ecourse <smkang@codenuri.co.kr>
2026-09-04 17:08:20 +0900

    Add files via upload

---
+++ DAY5/06_Linq5.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Linq 선수 지식 #2. coroutine
+
+class Program
+{
+    public static void foo()
+    {
+        Console.WriteLine("foo #1");
+        Console.WriteLine("foo #2");
+    }
+    public static void Main()
+    {
+        foo();
+    }
+}
