de479c4a81322917798bfd9a91c65ebf7c1f6b73
ecourse <smkang@codenuri.co.kr>
2026-09-04 10:39:39 +0900

    Add files via upload

--- DAY5/06_Linq2.cs
+++ DAY5/06_Linq2.cs
@@ -8,9 +8,11 @@ class Program
     {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
+        var c = arr.Where(n => n % 2 == 1);
 
-        var c = arr.Where(n => n % 2 == 1); 
+        arr[0] = 99;
 
+        // 아래 결과 예측해 보세요. 첫번째 출력이 "1" 일까요 ? "99" 일까요 ?
         foreach (var n in c)
         {
             Console.WriteLine(n);
