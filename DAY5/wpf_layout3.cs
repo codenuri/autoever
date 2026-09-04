00333fb49fd10018b4421cb8b909e6d3fa9811be
ecourse <smkang@codenuri.co.kr>
2026-09-04 12:16:29 +0900

    Add files via upload

--- DAY5/wpf_layout3.cs
+++ DAY5/wpf_layout3.cs
@@ -13,9 +13,21 @@ class MainWindow : Window
         Content = grid;
 
         // #2. grid 의 row 와 column 은 몇개인가 ?
-        grid.Row = 2;
-        grid.Column = 2;
- 
+        // 아래 처럼 하면 쉬워 보이지만, 각 row, column 의 속성 지정등이 불편합니다
+        // grid.Row = 2;
+        // grid.Column = 2;
+
+        RowDefinition r1 = new RowDefinition();
+        RowDefinition r2 = new RowDefinition();
+        ColumnDefinition c1 = new ColumnDefinition();
+        ColumnDefinition c2 = new ColumnDefinition();
+
+        // Row, Column 객체를 grid 에 추가 - 반복문 사용하면 편리
+        grid.RowDefinitions.Add(r1);
+        grid.RowDefinitions.Add(r2);
+        grid.ColumnDefinitions.Add(c1);
+        grid.ColumnDefinitions.Add(c2);
+
     }
 }
 
