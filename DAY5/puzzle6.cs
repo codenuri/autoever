b0d0e044258170a98ad94b21e0369293320f391c
ecourse <smkang@codenuri.co.kr>
2026-09-04 14:48:05 +0900

    Add files via upload

--- DAY5/puzzle6.cs
+++ DAY5/puzzle6.cs
@@ -106,8 +106,8 @@ class MainWindow : Window
         Point pt = e.GetPosition(this);
 
         // #1. 좌표로 부터 어느 블럭을 클릭했는지 계산
-        int bx = (int)(pt.X / block_width);
-        int by = (int)(pt.Y / block_height);
+        int bx = (int)(pt.X / (grid.ActualWidth / CNT));
+        int by = (int)(pt.Y / (grid.ActualHeight / CNT));
 
         // #2. 상/하/좌/우 에 EMPTY(24) 가 있는지 확인
         if (bx < CNT - 1 && state[by, bx + 1] == EMPTY)  // 오른쪽이 EMPTY
