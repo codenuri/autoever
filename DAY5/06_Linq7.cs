338ab13af9d4ba67db1ca6f9b091cdb2c2229a10
ecourse <smkang@codenuri.co.kr>
2026-09-04 18:09:36 +0900

    Add files via upload

--- DAY5/06_Linq7.cs
+++ DAY5/06_Linq7.cs
@@ -4,13 +4,25 @@ using System.Linq;
 
 // LINQ
 
+static class MyLinq
+{
+    public static int MyWhere(   )
+    {
+        return 0;
+    }
+}
+
+
+
+
 class Program
 {
     public static void Main()
     {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-        var c = arr.Where(n => n % 2 == 1); 
+//      var c = arr.Where(n => n % 2 == 1); 
+        var c = arr.MyWhere(n => n % 2 == 1);
 
         foreach (var n in c)
         {
@@ -18,3 +30,4 @@ class Program
         }
     }
 }
+
