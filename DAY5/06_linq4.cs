da9bc81d85a511c8dd3832fe3d82efe76466950d
ecourse <smkang@codenuri.co.kr>
2026-09-04 17:04:01 +0900

    Add files via upload

---
+++ DAY5/06_linq4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Linq 선수 지식 #1. foreach 원리
+
+class Program
+{
+    public static void Main()
+    {
+        int[] arr = { 1, 2, 3 };
+
+        foreach (int i in arr)
+        {
+            int n = i;  
+        }
+        // 위 코드는 컴파일러에 의해서 아래 코드로 변경됩니다.
+        var e = arr.GetEnumerator();
+
+        while( e.MoveNext() )
+        {
+            int n = e.Current;
+        }
+    }
+}
