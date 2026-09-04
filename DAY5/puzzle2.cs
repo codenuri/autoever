94cf2828e20fa12164435af69abd267b192a0a44
ecourse <smkang@codenuri.co.kr>
2026-09-04 12:38:16 +0900

    Add files via upload

---
+++ DAY5/puzzle2.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+// Sliding Puzzle Game #2
+// => 전체 그림이 아닌 한블럭만 출력하기
+
+class MainWindow : Window
+{
+    private const int CNT = 5;               // 5 * 5 게임
+    private const int EMPTY = CNT * CNT - 1; // 24번은 EMPTY
+
+    // 한개 블럭의 크기 관리
+    private double block_width;
+    private double block_height;
+
+
+
+    public MainWindow()
+    {
+        Uri uri = new Uri("..\\..\\..\\totoro.jpg", UriKind.Relative);
+
+        BitmapImage bitmap = new BitmapImage(uri);
+
+
+
+
+
+
+        Image img = new Image();
+        img.Source = bitmap;
+        img.Stretch = Stretch.Fill;
+
+
+        Content = img;
+    }
+}
+
+
+class App : Application
+{
+    [STAThread]
+    public static void Main()
+    {
+        App app = new App();
+
+        MainWindow w = new MainWindow();
+        w.Title = "Hello, WPF";
+        w.Show();
+
+        app.Run();
+    }
+}
