5bfd5d8ffd71e5791d2f187b8d78591d501a7b18
ecourse <smkang@codenuri.co.kr>
2026-09-04 11:46:31 +0900

    Add files via upload

--- DAY5/wpf_layout1.cs
+++ DAY5/wpf_layout1.cs
@@ -2,6 +2,8 @@ using System.Windows;
 using System.Windows.Controls;
 
 // #1. DockPanel 사용하기
+// => 자식 컨트롤을 "Top, Bottom, Left, Right" 에 부착
+// => 마지막 붙인 자식이 나머지 영역 전체를 사용
 
 class MainWindow : Window
 {
@@ -17,14 +19,17 @@ class MainWindow : Window
         Button btn3 = new Button { Content = "button3" };
 
         // #3. 각 컨트롤이 Dock 패널 어디에 놓일지 속성지정
+        // => 아직 자식 콘트롤로 등록된것은 아니고
+        // => 자식이 될때 어느위치에 들어갈지만 먼저 세팅
+        // => DockPanel 의 정적 메소드 사용
         DockPanel.SetDock(btn1, Dock.Top);
         DockPanel.SetDock(btn2, Dock.Bottom);
         DockPanel.SetDock(btn3, Dock.Left);
 
         // #4. 각 컨트롤을 패널에 자식으로 추가
         dp.Children.Add(btn1);
-        dp.Children.Add(btn2);
         dp.Children.Add(btn3);
+        dp.Children.Add(btn2); // <= 마지막 자식이 나머지 영역  전체 사용
 
     }
 }
