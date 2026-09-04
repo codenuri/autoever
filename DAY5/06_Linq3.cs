59a156f94ea45823ff1f9a5a841c680faed2265e
ecourse <smkang@codenuri.co.kr>
2026-09-04 10:46:47 +0900

    Add files via upload

---
+++ DAY5/06_Linq3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] arr = { "kim", "park", "choi", "lee", "jung" };
+
+        var c = arr.Where(s => s.Contains('i'))
+                   .OrderBy(s => s.Length)
+                   .Select(s => s.ToUpper());
+
+        foreach(var n in c)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
