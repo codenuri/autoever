c9422849112bb4d0f777898bf6b62a8c54571da9
ecourse <smkang@codenuri.co.kr>
2026-09-04 12:22:20 +0900

    Add files via upload

---
+++ DAY5/puzzle1.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+// Sliding Puzzle Game #1
+// => 화면에 그림 출력하기
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+    }
+}
+
+
+class App : Application
+{
+    [STAThread]
+    public static void Main()
+    {
+        App app = new App();
+
+        MainWindow w = new MainWindow();
+        w.Title = "Hello, WPF";
+        w.Show();
+
+        app.Run();
+    }
+}
