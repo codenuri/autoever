9f3a619d2cfae906e045bca1c2f07fc08132f74d
ecourse <smkang@codenuri.co.kr>
2026-09-04 11:31:43 +0900

    Add files via upload

---
+++ DAY5/05_using3.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        // ~ C# 8.0
+        // using (....)
+        // {
+        // }
+
+        // C# 9.0 아래 처럼 가능 - () 아 {} 없어도가능
+
+        using FileStream fs = new FileStream("a.txt", FileMode.Create);
+  
+        byte[] data = Encoding.UTF8.GetBytes("ABCD");
+
+        fs.Write(data, 0, data.Length);         
+
+    } // <-- fs.Dispose()
+}
+
