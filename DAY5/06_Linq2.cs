bbb867c30d346828cad4318e350a3ad4efd147d8
ecourse <smkang@codenuri.co.kr>
2026-09-04 10:38:44 +0900

    Add files via upload

---
+++ DAY5/06_Linq2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+        var c = arr.Where(n => n % 2 == 1); 
+
+        foreach (var n in c)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
