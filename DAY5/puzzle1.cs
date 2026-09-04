944033a03d238f7a8def97d379f664292ed4369e
ecourse <smkang@codenuri.co.kr>
2026-09-04 12:25:59 +0900

    Add files via upload

--- DAY5/puzzle1.cs
+++ DAY5/puzzle1.cs
@@ -8,6 +8,18 @@ class MainWindow : Window
 {
     public MainWindow()
     {
+        // #1. 자원의 위치를 관리하는 객체 생성
+        Uri uri = new Uri("..\\..\\..\\totoro.jpg"); // 여러분의 그림파일이름
+
+        // #2. 그림을 메모리에 Load
+        BitmapImage bitmap = new BitmapImage(uri);
+
+        // #3. bitmap 안에 있는 그림을 화면에 그릴때 사용하는 Image 객체 생성
+        Image img = new Image();
+        img.Source = bitmap;
+
+        // #4. Image 객체를 MainWindow Content 로 연결
+        Content = img;
     }
 }
 
